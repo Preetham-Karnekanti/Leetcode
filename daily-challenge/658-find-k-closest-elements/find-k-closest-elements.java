c5f5738195c789f83a06b47b51ca5c02b2010ed1
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-04-06 09:49:35 +0530

    Time: 4 ms (95.50%) | Memory: 45.7 MB (72.46%) - LeetSync

--- daily-challenge/658-find-k-closest-elements/find-k-closest-elements.java
+++ daily-challenge/658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,17 +1,19 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] a, int k, int x) {
-        List<Integer> nm = new ArrayList<>();
-        int i = 0, j = a.length - 1;
+    public List<Integer> findClosestElements(int[] nums, int k, int x) {
+        int i = 0;
+        int j = nums.length - 1;
         while (j - i + 1 > k) {
-            if (Math.abs(x - a[i]) > Math.abs(x - a[j])) {
+            int l = Math.abs(nums[i] - x);
+            int r = Math.abs(nums[j] - x);
+            if (l > r)
                 i++;
-            } else if (Math.abs(x - a[i]) <= Math.abs(x - a[j])) {
+            else
                 j--;
-            }
         }
+        List<Integer> al = new ArrayList<>();
         for (int p = i; p <= j; p++) {
-            nm.add(a[p]);
+            al.add(nums[p]);
         }
-        return nm;
+        return al;
     }
 }
