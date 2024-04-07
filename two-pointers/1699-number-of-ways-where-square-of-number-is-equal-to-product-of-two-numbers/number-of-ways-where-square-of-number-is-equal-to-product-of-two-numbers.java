dafb15e5901c803db47c0ae1463da4ba3c72475b
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-04-07 11:20:17 +0530

    Time: 10 ms (98.41%) | Memory: 42.6 MB (90.48%) - LeetSync

--- two-pointers/1699-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
+++ two-pointers/1699-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
@@ -2,43 +2,39 @@ class Solution {
     public int numTriplets(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        return count(nums1 , nums2) + count(nums2 , nums1);
+        return (int)countWays(nums1, nums2) + (int)countWays(nums2, nums1);
     }
-    
-    public int count(int a[] , int b[]){
-        int n = a.length;
-        int m = b.length;
-        int count = 0;
-        for(int i=0;i<n;i++){
-            long x = (long)a[i]*a[i];
+
+    public long countWays(int[] nums1, int[] nums2) {
+        long count = 0;
+        for (int i = 0; i < nums1.length; i++) {
             int j = 0;
-            int k = m-1;
-            while(j<k){
-                long prod = (long)b[j]*b[k];
-                if(prod<x)
-                    j++;
-                else if(prod>x)
-                    k--;
-                else if(b[j] != b[k]){
+            int k = nums2.length - 1;
+            long square = (long)nums1[i] * (long)nums1[i];
+            while (j < k) {
+                long product = (long) nums2[j] * (long) nums2[k];
+                if (square == product) {
+                    if (nums2[j] == nums2[k]) {
+                        int q = k - j + 1;
+                        count = count + (q * (q - 1)) / 2;
+                        break;
+                    }
                     int jNew = j;
                     int kNew = k;
-                    
-                    while(b[j] == b[jNew])
+
+                    while (nums2[j] == nums2[jNew])
                         jNew++;
-                    while(b[k] == b[kNew])
+                    while (nums2[k] == nums2[kNew])
                         kNew--;
-                    count += (jNew-j)*(k-kNew);
+                    count += (jNew - j) * (k - kNew);
                     j = jNew;
                     k = kNew;
-                }
-                else{
-                    int q = k-j+1;
-                    count += (q)*(q-1)/2;
-                    break;
-                }
+                } else if (square > product)
+                    j++;
+                else
+                    k--;
             }
         }
         return count;
     }
-    
 }
