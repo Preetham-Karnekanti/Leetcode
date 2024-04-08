39077e80b111786e9f3f57ac8175e76b71793a62
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-04-08 19:35:45 +0530

    Time: 50 ms (33.07%) | Memory: 85.8 MB (57.09%) - LeetSync

--- hashing/1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
+++ hashing/1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -1,21 +1,23 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
+        int n = s.length();
+        char[] ch = s.toCharArray();
         HashMap<String, String> hm = new HashMap<>();
         for (int i = 0; i < knowledge.size(); i++) {
             hm.put(knowledge.get(i).get(0), knowledge.get(i).get(1));
         }
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
+        for (int i = 0; i < n; i++) {
+            if (ch[i] == '(') {
                 String key = "";
                 int j = i + 1;
-                while (j < s.length() && s.charAt(j) != ')')
+                while (j < n && ch[j] != ')')
                     key += s.charAt(j++);
                 i = j;
                 String value = hm.get(key) == null ? "?" : hm.get(key);
                 sb.append(value);
             } else
-                sb.append(s.charAt(i));
+                sb.append(ch[i]);
         }
         return sb.toString();
     }
