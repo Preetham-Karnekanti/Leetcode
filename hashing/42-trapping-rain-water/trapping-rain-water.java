395e416550759186cb7770f2dcc84e3e5d6253f0
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-04-12 10:51:18 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (7.14%) - LeetSync

--- hashing/42-trapping-rain-water/trapping-rain-water.java
+++ hashing/42-trapping-rain-water/trapping-rain-water.java
@@ -1,26 +1,19 @@
 class Solution {
     public int trap(int[] height) {
-        int left[] = new int[height.length];
-        int right[] = new int[height.length];
-
-        left[0] = height[0];
-
-        for (int i = 1; i < height.length; i++) {
-            left[i] = Math.max(left[i - 1], height[i]);
-        }
-        int n = height.length;
-        right[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = Math.max(right[i + 1], height[i]);
-        }
-
-        System.out.println(Arrays.toString(left));
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int min = Math.min(left[i], right[i]);
-            ans += min - height[i];
+        int l = 0, r = height.length - 1, sum = 0, leftBar = height[l], rightBar = height[r];
+        
+        while (l < r) {
+            if (leftBar <= rightBar) {
+                sum += leftBar - height[l];
+                l++;
+                leftBar = Math.max(leftBar, height[l]);
+            } else {
+                sum += rightBar - height[r];
+                r--;
+                rightBar = Math.max(rightBar, height[r]);
+            }
         }
 
-        return ans;
+        return sum;
     }
 }
