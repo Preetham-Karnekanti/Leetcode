d271f37dbfd84634bd74c07d588eac6499f3754e
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-04-16 11:49:58 +0530

    Time: 6 ms (91.04%) | Memory: 45.2 MB (76.71%) - LeetSync

---
+++ hashing/835-linked-list-components/linked-list-components.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        if(nums.length == 1)
+            return 1;
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i : nums)
+            hs.add(i);
+        int count = 0;
+        while (head != null) {
+            boolean hasPair = false;
+            while (head != null && hs.contains(head.val)) {
+                head = head.next;
+                hasPair = true;
+            }
+            if(hasPair)
+                count++;
+            if (head != null)
+                head = head.next;
+        }
+        return count;
+    }
+}
