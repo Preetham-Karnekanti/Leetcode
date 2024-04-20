0556dcdcffbf081840b07a6fb5bd2f42b4c33c71
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-04-20 17:26:25 +0530

    Time: 112 ms (13.24%) | Memory: 45.3 MB (73.97%) - LeetSync

---
+++ stacks/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int count = 0;
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!st.empty() && s.charAt(i) == 'a' && st.peek() == 'b') {
+                count++;
+                st.pop();
+            }
+            else if (s.charAt(i) == 'b')
+                st.push('b');
+        }
+        return count;
+    }
+}
