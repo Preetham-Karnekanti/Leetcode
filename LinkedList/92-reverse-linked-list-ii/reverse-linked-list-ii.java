f035b6c796536406f34143b1df0d3a24c793f1b6
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-04-24 19:05:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (49.12%) - LeetSync

--- LinkedList/92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ LinkedList/92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,26 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    private static ListNode reverse(ListNode head, int n) {
-        ListNode temp = head, newHead = null, node = null;
-        for (int i = 0; i < n; i++) {
-            node = temp;
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        int count = right - left + 1;
+        if (left == 1)
+            return reverse(head, count);
+        ListNode temp = head;
+
+        for (int i = 1; i < left - 1; i++) {
             temp = temp.next;
-            node.next = newHead;
-            newHead = node;
         }
-        head.next = temp;
-        return newHead;
+        temp.next = reverse(temp.next, count);
+        return head;
     }
 
-    public ListNode reverseBetween(ListNode head, int l, int r) {
-        int n = r - l + 1;
-        if (n < 2)
-            return head;
-        if (l == 1)
-            return reverse(head, n);
-        ListNode temp = head;
-        for (int i = 2; i < l; i++)
-            temp = temp.next;
-        temp.next = reverse(temp.next, n);
-        return head;
+    public ListNode reverse(ListNode head, int count) {
+        ListNode cur = head, prev = null;
+        while (cur != null && count > 0) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+            count--;
+        }
+        head.next = cur;
+        return prev;
     }
 }
