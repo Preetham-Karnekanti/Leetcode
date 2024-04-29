aa815d7a3b54bbd45671aa745cd08d5eef62ef28
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-04-29 09:53:36 +0530

    Time: 8 ms (47.34%) | Memory: 50.2 MB (24.15%) - LeetSync

--- Trees/2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ Trees/2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -1,35 +1,39 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        if (root == null)
-            return null;
-        helper(root.left, root.right, 1);
+        if (root == null) {
+            return root;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                TreeNode node = q.poll();
+                if (node.left != null)
+                    q.offer(node.left);
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+
+            level++;
+            if (level % 2 == 1 && !q.isEmpty()) {
+                int[] nums = new int[q.size()];
+                int i = 0;
+                for (TreeNode node : q) {
+                    nums[i++] = node.val;
+                }
+
+                int j = q.size() - 1;
+                for (TreeNode node : q) {
+                    node.val = nums[j--];
+                }
+            }
+        }
+
         return root;
     }
-
-    public void helper(TreeNode root1, TreeNode root2, int level) {
-        if (root1 == null || root2 == null)
-            return;
-        if (level % 2 == 1) {
-            int temp = root1.val;
-            root1.val = root2.val;
-            root2.val = temp;
-        }
-        helper(root1.left, root2.right, level + 1);
-        helper(root1.right, root2.left, level + 1);
-    }
 }
+
+// TC: O(n), SC: O(n)
