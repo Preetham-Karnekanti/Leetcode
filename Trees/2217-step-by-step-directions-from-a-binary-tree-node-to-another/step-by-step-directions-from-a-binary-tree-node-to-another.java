03bc26d11ba5986bb1e2751598032d8c6e629f28
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-03 14:51:26 +0530

    Time: 830 ms (5.06%) | Memory: 85.8 MB (23.69%) - LeetSync

--- Trees/2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ Trees/2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -1,73 +1,68 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
-class Solution {
-    TreeNode startNode = null;
-
-    public String getDirections(TreeNode root, int startValue, int destValue) {
-        HashMap<TreeNode, TreeNode> childToParent = new HashMap<>();
-        getParent(root, startValue, childToParent);
-        Queue<Pair> q = new LinkedList<>();
-        HashSet<TreeNode> visited = new HashSet<>();
-        q.add(new Pair(startNode, ""));
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
-            if (p.node.val == destValue)
-                return p.path;
-            if (p.node.left != null && !visited.contains(p.node.left)) {
-                visited.add(p.node.left);
-                q.add(new Pair(p.node.left, p.path + "L"));
-            }
-            if (p.node.right != null && !visited.contains(p.node.right)) {
-                visited.add(p.node.right);
-                q.add(new Pair(p.node.right, p.path + "R"));
-            }
-            TreeNode parent = childToParent.get(p.node);
-            if (parent != null && !visited.contains(parent)) {
-                visited.add(parent);
-                q.add(new Pair(parent, p.path + "U"));
-            }
-        }
-        return null;
-    }
-
-    public void getParent(TreeNode root, int startValue, HashMap<TreeNode, TreeNode> childToParent) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode node = q.poll();
-            if (node.val == startValue)
-                startNode = node;
-            if (node.left != null) {
-                q.add(node.left);
-                childToParent.put(node.left, node);
-            }
-            if (node.right != null) {
-                q.add(node.right);
-                childToParent.put(node.right, node);
-            }
-        }
+class Pair{
+    StringBuilder str;
+    TreeNode root;
+    public Pair(StringBuilder str, TreeNode root){
+        this.str = str;
+        this.root = root;
     }
 }
+class Solution {
+    HashMap<TreeNode, TreeNode> parent;
+    TreeNode src;
+    HashSet<Integer> visited;
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        parent = new HashMap<>();
+        src = null;
+        visited = new HashSet<>();
+        inorder(root, null, startValue);
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(new StringBuilder(""), src));
+        visited.add(src.val);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                Pair pair = queue.remove();
+                TreeNode node = pair.root;
+                StringBuilder str = pair.str;  
+                if(node.left != null && !visited.contains(node.left.val)){
+                    StringBuilder dummy = new StringBuilder(str);
+                    dummy.append("L");
+                    visited.add(node.val);
+                    if(node.left.val == destValue)
+                        return dummy.toString();
+                    queue.add(new Pair(dummy, node.left));
+                }
+                if(node.right != null && !visited.contains(node.right.val)){
+                    StringBuilder dummy = new StringBuilder(str);
+                    dummy.append("R");
+                    visited.add(node.val);
+                    if(node.right.val == destValue)
+                        return dummy.toString();
+                    queue.add(new Pair(dummy, node.right));
+                }
+                if(parent.get(node) != null && !visited.contains(parent.get(node).val)){
+                    StringBuilder dummy = new StringBuilder(str);
+                    dummy.append("U");
+                    visited.add(parent.get(node).val);
+                    if(parent.get(node).val == destValue)
+                        return dummy.toString();
+                    queue.add(new Pair(dummy, parent.get(node)));
+                }
+            }
+        }
+        return "";
+    }
 
-class Pair {
-    String path;
-    TreeNode node;
-
-    Pair(TreeNode node, String path) {
-        this.path = path;
-        this.node = node;
+        
+    public void inorder(TreeNode root, TreeNode par, int startVal){
+        if(root == null)
+            return;
+        if(root.left != null)
+            inorder(root.left, root, startVal);
+        parent.put(root, par);
+        if(root.val == startVal)
+            src = root;
+        if(root.right != null)
+            inorder(root.right, root, startVal);
     }
 }
