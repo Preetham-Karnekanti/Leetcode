4f41ac92d758426ad31945c1b40170cc284d78ff
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-06 11:40:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (45.11%) - LeetSync

--- Trees/832-binary-tree-pruning/binary-tree-pruning.java
+++ Trees/832-binary-tree-pruning/binary-tree-pruning.java
@@ -15,9 +15,8 @@
  */
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
-        if (root == null) {
+        if (root == null)
             return null;
-        }
         root.left = pruneTree(root.left);
         root.right = pruneTree(root.right);
         if (root.val != 1 && root.left == null && root.right == null)
