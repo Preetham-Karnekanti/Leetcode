79d354edcf0e17217b2ce07bbf9d8c9cd635f1c9
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-08 11:54:20 +0530

    Time: 11 ms (5.55%) | Memory: 46.5 MB (26.86%) - LeetSync

---
+++ Trees/813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        Queue<List<Integer>> q = new LinkedList<>();
+        q.add(new ArrayList<>(Arrays.asList(0)));
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        while (!q.isEmpty()) {
+            List<Integer> path = q.poll();
+            if (path.get(path.size() - 1) == graph.length - 1) {
+                ans.add(new ArrayList<>(path));
+            } else {
+                for (int x : graph[path.get(path.size() - 1)]) {
+                    List<Integer> newPath = new ArrayList<>(path);
+                    newPath.add(x);
+                    q.add(newPath);
+                }
+            }
+        }
+        return ans;
+    }
+}
