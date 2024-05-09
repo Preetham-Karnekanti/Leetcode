f896cb682790eef30dcf9d643f6513136693bcb7
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-09 15:36:39 +0530

    Time: 5 ms (62.31%) | Memory: 44.8 MB (53.08%) - LeetSync

--- Trees/1493-frog-position-after-t-seconds/frog-position-after-t-seconds.java
+++ Trees/1493-frog-position-after-t-seconds/frog-position-after-t-seconds.java
@@ -1,38 +1,36 @@
 class Solution {
     public double frogPosition(int n, int[][] edges, int t, int target) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> al = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
-            adj.add(new ArrayList<>());
+            al.add(new ArrayList<>());
         }
-        for (int edge[] : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+        for (int e[] : edges) {
+            int u = e[0];
+            int v = e[1];
+            al.get(v).add(u);
+            al.get(u).add(v);
         }
-        int timeSoFar = 0;
-        boolean visited[] = new boolean[n + 1];
-        visited[1] = true;
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(1, 1, 0));
+        boolean visited[] = new boolean[n + 1];
+        q.add(new Pair(1, 0, 1));
+        visited[1] = true;
         while (!q.isEmpty()) {
             Pair p = q.poll();
-            if (p.time == t && p.node == target) {
+            if (p.time == t && p.node == target)
                 return p.probability;
-            }
-            double children = adj.get(p.node).size();
+            int children = al.get(p.node).size();
             if (p.node != 1)
                 children--;
             if (children == 0) {
-                if (p.node == target && p.time <= t)
+                if (target == p.node && p.time <= t)
                     return p.probability;
             }
-            for (int child : adj.get(p.node)) {
+            for (int child : al.get(p.node)) {
                 if (!visited[child]) {
                     visited[child] = true;
                     int time = p.time + 1;
-                    double probability = p.probability * (1 / children);
-                    q.add(new Pair(child, probability, time));
+                    double probability = p.probability / children;
+                    q.add(new Pair(child, time, probability));
                 }
             }
         }
@@ -41,13 +39,13 @@ class Solution {
 }
 
 class Pair {
-    int node;
+    int node, time;
     double probability;
-    int time;
 
-    Pair(int cur, double probability, int time) {
-        this.node = cur;
+    Pair(int node, int time, double probability) {
         this.probability = probability;
+        this.node = node;
         this.time = time;
     }
+
 }
