daf813cd4d0fae79cb60e5e379831bf26ae719b6
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-15 16:55:03 +0530

    Time: 271 ms (36.57%) | Memory: 45.4 MB (52.00%) - LeetSync

---
+++ heaps/2422-query-kth-smallest-trimmed-number/query-kth-smallest-trimmed-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] smallestTrimmedNumbers(String[] nums, int[][] queries) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+                (a, b) -> b.val.equals(a.val) ? b.idx - a.idx : b.val.compareTo(a.val));
+        int ans[] = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int k = queries[i][0];
+            int x = queries[i][1];
+            for (int j = 0; j < nums.length; j++) {
+                String temp = nums[j].substring(nums[j].length() - x);
+                pq.add(new Pair(temp, j));
+                if (pq.size() > k)
+                    pq.poll();
+            }
+            ans[i] = pq.peek().idx;
+            pq.clear();
+        }
+        return ans;
+    }
+}
+
+class Pair {
+    int idx;
+    String val;
+
+    Pair(String v, int idx) {
+        val = v;
+        this.idx = idx;
+    }
+}
