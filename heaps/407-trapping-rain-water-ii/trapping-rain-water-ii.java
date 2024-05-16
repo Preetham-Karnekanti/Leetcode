9f3ba4be07fe14a01a724817f41623f03ae1018a
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-17 02:46:21 +0530

    Time: 29 ms (29.94%) | Memory: 45.9 MB (79.45%) - LeetSync

---
+++ heaps/407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int trapRainWater(int[][] heightMap) {
+        int dx[] = new int[] { 1, -1, 0, 0 };
+        int dy[] = new int[] { 0, 0, -1, 1 };
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            // if (a[0] != b[0])
+                return a[0] - b[0];
+            // if (a[1] != b[1])
+                // return a[1] - b[1];
+            // return a[2] - b[2];
+        });
+        int rows = heightMap.length;
+        int cols = heightMap[0].length;
+        boolean[][] vis = new boolean[rows][cols];
+        int minHeight = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == 0 || j == 0 || i == rows - 1 || j == cols-1) {
+                    pq.add(new int[] { heightMap[i][j], i, j });
+                    vis[i][j] = true;
+                }
+            }
+        }
+        int res = 0;
+        while (!pq.isEmpty()) {
+            int cur[] = pq.poll();
+            minHeight = Math.max(minHeight, cur[0]);
+            int r = cur[1];
+            int c = cur[2];
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+                if (!isValid(nr, nc, rows, cols) || vis[nr][nc])
+                    continue;
+                pq.add(new int[] { heightMap[nr][nc], nr, nc });
+                vis[nr][nc] = true;
+                if (heightMap[nr][nc] < minHeight) {
+                    res += (minHeight - heightMap[nr][nc]);
+                }
+            }
+        }
+        return res;
+    }
+
+    public boolean isValid(int i, int j, int r, int c) {
+        if (i < 0 || j < 0 || i >= r || j >= c)
+            return false;
+        return true;
+    }
+}
