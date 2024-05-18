2addf1a71647bb61d899ab45ebd08728e789e5e5
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-19 00:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (56.87%) - LeetSync

---
+++ greedy/1858-latest-time-by-replacing-hidden-digits/latest-time-by-replacing-hidden-digits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String maximumTime(String time) {
+        char[] ch = time.toCharArray();
+        if (ch[0] == '?') {
+            if(ch[1] == '?')
+                ch[0] = '2';
+            else if (ch[1] >= '4')
+                ch[0] = '1';
+            else
+                ch[0] = '2';
+        }
+        if (ch[1] == '?') {
+            if (ch[0] == '1')
+                ch[1] = '9';
+            else if(ch[0] == '2')
+                ch[1] = '3';
+            else
+                ch[1] = '9';
+        }
+        if (ch[3] == '?')
+            ch[3] = '5';
+        if (ch[4] == '?')
+            ch[4] = '9';
+        return new String(ch);
+    }
+}
