f96c1f076ffc4238c4e3e8d51c2cd98cd7acd772
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-22 12:24:55 +0530

    Time: 13 ms (11.33%) | Memory: 54.6 MB (78.83%) - LeetSync

---
+++ dynamicProgramming/1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -0,0 +1,45 @@
+class Solution {
+    int dx[];
+    int dy[];
+    int dp[][];
+
+    public int countSquares(int[][] matrix) {
+        dx = new int[] { 1, 1, 0 };
+        dy = new int[] { 0, 1, 1 };
+        dp = new int[matrix.length][matrix[0].length];
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++)
+            Arrays.fill(dp[i], -1);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                count += dfs(matrix, i, j);
+            }
+        }
+        return count;
+    }
+
+    public int dfs(int[][] matrix, int x, int y) {
+        if (!isValid(x, y, matrix.length, matrix[0].length))
+            return 0;
+        if (matrix[x][y] == 0) {
+            dp[x][y] = 0;
+            return 0;
+        }
+        if (dp[x][y] != -1)
+            return dp[x][y];
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            min = Math.min(min, dfs(matrix, nx, ny));
+        }
+        dp[x][y] = 1 + min;
+        return dp[x][y];
+    }
+
+    public boolean isValid(int r, int c, int rows, int cols) {
+        if (r < 0 || c < 0 || r >= rows || c >= cols)
+            return false;
+        return true;
+    }
+}
