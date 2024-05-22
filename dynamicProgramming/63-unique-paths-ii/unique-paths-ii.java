d94d424e2a8e92148bb0f8d618588c93ebcb3954
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-22 23:48:32 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (10.41%) - LeetSync

---
+++ dynamicProgramming/63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    int dp[][];
+
+    public int uniquePathsWithObstacles(int[][] grid) {
+        dp = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++)
+            Arrays.fill(dp[i], -1);
+        return helper(grid, 0, 0);
+    }
+
+    public int helper(int[][] grid, int i, int j) {
+        if (i >= grid.length || j >= grid[0].length || grid[i][j] == 1)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (i == grid.length - 1 && j == grid[0].length - 1)
+            return 1;
+        return dp[i][j] = helper(grid, i + 1, j) + helper(grid, i, j + 1);
+    }
+}
