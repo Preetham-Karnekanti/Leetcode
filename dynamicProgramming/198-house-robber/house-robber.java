5008caf59abb3e831db513a0852445ca05742245
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-22 15:26:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (11.99%) - LeetSync

---
+++ dynamicProgramming/198-house-robber/house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    int dp[];
+
+    public int rob(int[] nums) {
+        dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return Math.max(helper(nums, 0), helper(nums, 1));
+    }
+
+    public int helper(int nums[], int idx) {
+        if (idx >= nums.length)
+            return 0;
+        if (dp[idx] != -1)
+            return dp[idx];
+        int take = nums[idx] + helper(nums, idx + 2);
+        int notTake = helper(nums, idx + 1);
+        return dp[idx] = Math.max(take, notTake);
+    }
+}
