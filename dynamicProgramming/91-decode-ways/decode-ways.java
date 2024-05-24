a70a008d41fd13789c79b9e2b1c9f748151ce43e
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-25 00:57:20 +0530

    Time: 1 ms (80.05%) | Memory: 41.7 MB (54.14%) - LeetSync

--- dynamicProgramming/91-decode-ways/decode-ways.java
+++ dynamicProgramming/91-decode-ways/decode-ways.java
@@ -1,27 +1,26 @@
 class Solution {
-    int dp[];
-
     public int numDecodings(String s) {
-        dp = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return helper(s, 0);
-    }
-
-    public int helper(String s, int idx) {
-        if (idx >= s.length()) {
-            return 1;
-        }
-        if (s.charAt(idx) == '0')
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
             return 0;
-        if (dp[idx] != -1)
-            return dp[idx];
-        int count = helper(s, idx + 1);
-        if (idx + 1 < s.length() && (s.charAt(idx) == '1' || s.charAt(idx) == '2' && s.charAt(idx + 1) < '7')) {
-            String num = "" + s.charAt(idx) + s.charAt(idx + 1);
-            int num1 = Integer.parseInt(num);
-            if (num1 <= 26)
-                count += helper(s, idx + 2);
         }
-        return dp[idx] = count;
+        int n = s.length();
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            int oneDigit = s.charAt(i - 1) - '0';
+            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+
+            if (oneDigit != 0) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (10 <= twoDigits && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
     }
 }
