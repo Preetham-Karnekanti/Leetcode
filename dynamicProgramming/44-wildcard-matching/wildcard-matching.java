9962a699a9ef30990690af395ae900bb57b52bb6
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-25 17:18:21 +0530

    Time: 16 ms (80.74%) | Memory: 48.5 MB (16.98%) - LeetSync

--- dynamicProgramming/44-wildcard-matching/wildcard-matching.java
+++ dynamicProgramming/44-wildcard-matching/wildcard-matching.java
@@ -2,31 +2,31 @@ class Solution {
     int dp[][];
 
     public boolean isMatch(String s, String p) {
-        dp = new int[p.length()][s.length()];
+        dp = new int[p.length() + 1][s.length() + 1];
         for (int i[] : dp)
             Arrays.fill(i, -1);
-        return helper(p, s, p.length()-1, s.length()-1);
+        return helper(p, s, p.length(), s.length());
     }
 
     public boolean helper(String p, String s, int i, int j) {
-        if (i < 0 && j < 0)
+        if (i == 0 && j == 0)
             return true;
-        if (i < 0 && j >= 0)
+        if (i == 0 && j > 0)
             return false;
-        if (j < 0 && i >= 0) {
-            for (int ii = 0; ii <= i; ii++)
+        if (j == 0 && i > 0) {
+            for (int ii = 0; ii < i; ii++)
                 if (p.charAt(ii) != '*')
                     return false;
             return true;
         }
         if (dp[i][j] != -1)
             return dp[i][j] == 0 ? false : true;
-        if (p.charAt(i) == s.charAt(j) || p.charAt(i) == '?') {
+        if (p.charAt(i - 1) == s.charAt(j - 1) || p.charAt(i - 1) == '?') {
             boolean val = helper(p, s, i - 1, j - 1);
             dp[i][j] = val == false ? 0 : 1;
             return val;
         }
-        if (p.charAt(i) == '*') {
+        if (p.charAt(i - 1) == '*') {
             boolean val = helper(p, s, i - 1, j) || helper(p, s, i, j - 1);
             dp[i][j] = val == false ? 0 : 1;
             return val;
