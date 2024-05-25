cab2234f83547a5a434dc9ece4625f3c5abf4ddd
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-25 12:45:47 +0530

    Time: 17 ms (20.14%) | Memory: 49.3 MB (14.48%) - LeetSync

--- dynamicProgramming/115-distinct-subsequences/distinct-subsequences.java
+++ dynamicProgramming/115-distinct-subsequences/distinct-subsequences.java
@@ -2,10 +2,22 @@ class Solution {
     int dp[][];
 
     public int numDistinct(String s, String t) {
-        dp = new int[s.length()][t.length()];
-        for (int i = 0; i < s.length(); i++)
-            Arrays.fill(dp[i], -1);
-        return helper(s, t, 0, 0);
+        dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j <= t.length(); j++) {
+                if (i == 0 || j == 0)
+                    continue;
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
     }
 
     public int helper(String s, String t, int i, int j) {
