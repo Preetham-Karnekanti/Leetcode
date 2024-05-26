82fc7a4d6acd9c72ce5d0a1b559c292addbed058
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-26 14:54:12 +0530

    Time: 9 ms (27.19%) | Memory: 44.7 MB (29.39%) - LeetSync

--- dynamicProgramming/188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ dynamicProgramming/188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,39 +1,40 @@
 class Solution {
-    int dp[][][];
 
     public int maxProfit(int k1, int[] prices) {
-        dp = new int[prices.length + 1][k1 + 1][2];
+        int prev[][] = new int[k1 + 1][2];
         for (int idx = prices.length - 1; idx >= 0; idx--) {
+            int cur[][] = new int[k1 + 1][2];
             for (int k = 1; k <= k1; k++) {
                 for (int buy = 0; buy <= 1; buy++) {
                     if (buy == 1) {
-                        int b = -prices[idx] + dp[idx + 1][k][0];
-                        int skip = dp[idx + 1][k][1];
-                        dp[idx][k][buy] = Math.max(b, skip);
+                        int b = -prices[idx] + prev[k][0];
+                        int skip = prev[k][1];
+                        cur[k][buy] = Math.max(b, skip);
                     } else {
-                        int s = prices[idx] + dp[idx + 1][k - 1][1];
-                        int skip = dp[idx + 1][k][0];
-                        dp[idx][k][buy] = Math.max(s, skip);
+                        int s = prices[idx] + prev[k - 1][1];
+                        int skip = prev[k][0];
+                        cur[k][buy] = Math.max(s, skip);
                     }
                 }
             }
+            prev = cur;
         }
-        return dp[0][k1][1];
+        return prev[k1][1];
     }
 
-    public int helper(int idx, int k, int buy, int[] prices) {
-        if (idx == prices.length || k == 0)
-            return 0;
-        if (dp[idx][k][buy] != -1)
-            return dp[idx][k][buy];
-        if (buy == 1) {
-            int b = -prices[idx] + helper(idx + 1, k, 0, prices);
-            int skip = helper(idx + 1, k, 1, prices);
-            return dp[idx][k][buy] = Math.max(b, skip);
-        } else {
-            int s = prices[idx] + helper(idx + 1, k - 1, 1, prices);
-            int skip = helper(idx + 1, k, 0, prices);
-            return dp[idx][k][buy] = Math.max(s, skip);
-        }
-    }
+    // public int helper(int idx, int k, int buy, int[] prices) {
+    // if (idx == prices.length || k == 0)
+    // return 0;
+    // if (dp[idx][k][buy] != -1)
+    // return dp[idx][k][buy];
+    // if (buy == 1) {
+    // int b = -prices[idx] + helper(idx + 1, k, 0, prices);
+    // int skip = helper(idx + 1, k, 1, prices);
+    // return dp[idx][k][buy] = Math.max(b, skip);
+    // } else {
+    // int s = prices[idx] + helper(idx + 1, k - 1, 1, prices);
+    // int skip = helper(idx + 1, k, 0, prices);
+    // return dp[idx][k][buy] = Math.max(s, skip);
+    // }
+    // }
 }
