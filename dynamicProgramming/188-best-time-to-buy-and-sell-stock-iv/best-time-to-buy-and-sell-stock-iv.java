c9aa6e0ce35c5d88b7a939b9ea122a94c625a15c
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-26 14:52:01 +0530

    Time: 9 ms (27.19%) | Memory: 48 MB (12.56%) - LeetSync

--- dynamicProgramming/188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ dynamicProgramming/188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,13 +1,24 @@
 class Solution {
     int dp[][][];
 
-    public int maxProfit(int k, int[] prices) {
-        dp = new int[prices.length][k + 1][2];
-        for (int[][] i : dp) {
-            for (int j[] : i)
-                Arrays.fill(j, -1);
+    public int maxProfit(int k1, int[] prices) {
+        dp = new int[prices.length + 1][k1 + 1][2];
+        for (int idx = prices.length - 1; idx >= 0; idx--) {
+            for (int k = 1; k <= k1; k++) {
+                for (int buy = 0; buy <= 1; buy++) {
+                    if (buy == 1) {
+                        int b = -prices[idx] + dp[idx + 1][k][0];
+                        int skip = dp[idx + 1][k][1];
+                        dp[idx][k][buy] = Math.max(b, skip);
+                    } else {
+                        int s = prices[idx] + dp[idx + 1][k - 1][1];
+                        int skip = dp[idx + 1][k][0];
+                        dp[idx][k][buy] = Math.max(s, skip);
+                    }
+                }
+            }
         }
-        return helper(0, k, 1, prices);
+        return dp[0][k1][1];
     }
 
     public int helper(int idx, int k, int buy, int[] prices) {
