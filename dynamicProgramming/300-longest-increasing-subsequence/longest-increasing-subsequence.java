7e9bb2201fbb7ae98116e42c0663e834d34e09c5
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-26 18:41:07 +0530

    Time: 75 ms (22.34%) | Memory: 44.3 MB (33.04%) - LeetSync

--- dynamicProgramming/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ dynamicProgramming/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,31 +1,33 @@
 class Solution {
-    int dp[][];
+    // int dp[][];
 
     public int lengthOfLIS(int[] nums) {
-        dp = new int[nums.length + 1][nums.length + 1];
+        int pre[] = new int[nums.length + 1];
         for (int idx = nums.length - 1; idx >= 0; idx--) {
+            int cur[] = new int[nums.length + 1];
             for (int prev = idx - 1; prev >= -1; prev--) {
-                int notTake = dp[idx + 1][prev + 1];
+                int notTake = pre[prev + 1];
                 int take = 0;
                 if (prev == -1 || nums[idx] > nums[prev]) {
-                    take = 1 + dp[idx + 1][idx + 1];
+                    take = 1 + pre[idx + 1];
                 }
-                dp[idx][prev + 1] = Math.max(take, notTake);
+                cur[prev + 1] = Math.max(take, notTake);
             }
+            pre = cur;
         }
-        return dp[0][0];
+        return pre[0];
     }
 
-    public int helper(int nums[], int idx, int prev) {
-        if (idx == nums.length)
-            return 0;
-        if (dp[idx][prev + 1] != -1)
-            return dp[idx][prev + 1];
-        int notTake = helper(nums, idx + 1, prev);
-        int take = 0;
-        if (prev == -1 || nums[idx] > nums[prev]) {
-            take = 1 + helper(nums, idx + 1, idx);
-        }
-        return dp[idx][prev + 1] = Math.max(take, notTake);
-    }
+    // public int helper(int nums[], int idx, int prev) {
+    // if (idx == nums.length)
+    // return 0;
+    // if (dp[idx][prev + 1] != -1)
+    // return dp[idx][prev + 1];
+    // int notTake = helper(nums, idx + 1, prev);
+    // int take = 0;
+    // if (prev == -1 || nums[idx] > nums[prev]) {
+    // take = 1 + helper(nums, idx + 1, idx);
+    // }
+    // return dp[idx][prev + 1] = Math.max(take, notTake);
+    // }
 }
