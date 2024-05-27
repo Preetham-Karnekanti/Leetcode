db41a61bffdb7dd5bf97132f830bcaee9e9602f9
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-27 18:54:34 +0530

    Time: 10 ms (96.58%) | Memory: 42.2 MB (79.48%) - LeetSync

--- dynamicProgramming/1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ dynamicProgramming/1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -8,23 +8,36 @@ class Solution {
             cost[i + 1] = cuts[i];
         cost[0] = 0;
         cost[len + 1] = n;
-        dp = new int[cuts.length + 1][cuts.length + 1];
-        for (int i[] : dp)
-            Arrays.fill(i, -1);
+        dp = new int[cuts.length + 2][cuts.length + 2];
+
         Arrays.sort(cost);
-        return helper(cost, 1, cuts.length);
+        for (int i = len; i >= 1; i--) {
+            for (int j = 1; j <= len; j++) {
+                if (i > j)
+                    continue;
+                int min = (int) 1e9;
+                for (int k = i; k <= j; k++) {
+                    int temp = cost[j + 1] - cost[i - 1]
+                            + dp[i][k - 1] + dp[k + 1][j];
+                    min = Math.min(temp, min);
+                }
+                dp[i][j] = min;
+            }
+        }
+        return dp[1][cuts.length];
     }
 
-    public int helper(int[] cuts, int i, int j) {
-        if (i > j)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int min = (int) 1e9;
-        for (int k = i; k <= j; k++) {
-            int cost = cuts[j + 1] - cuts[i - 1] + helper(cuts, i, k - 1) + helper(cuts, k + 1, j);
-            min = Math.min(cost, min);
-        }
-        return dp[i][j] = min;
-    }
+    // public int helper(int[] cuts, int i, int j) {
+    // if (i > j)
+    // return 0;
+    // if (dp[i][j] != -1)
+    // return dp[i][j];
+    // int min = (int) 1e9;
+    // for (int k = i; k <= j; k++) {
+    // int cost = cuts[j + 1] - cuts[i - 1] + helper(cuts, i, k - 1) + helper(cuts,
+    // k + 1, j);
+    // min = Math.min(cost, min);
+    // }
+    // return dp[i][j] = min;
+    // }
 }
