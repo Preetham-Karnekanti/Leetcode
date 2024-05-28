3df7e799367bfa2901d8400e8b4c389cb4e1c523
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-28 22:09:15 +0530

    Time: 1 ms (99.31%) | Memory: 43.8 MB (79.15%) - LeetSync

--- dynamicProgramming/120-triangle/triangle.java
+++ dynamicProgramming/120-triangle/triangle.java
@@ -2,29 +2,17 @@ class Solution {
     int dp[][];
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[] front = new int[n];
-        for (int i = 0; i < n; i++)
-            front[i] = triangle.get(n - 1).get(i);
-        for (int i = n - 2; i >= 0; i--) {
-            int[] cur = new int[i + 1];
-            for (int j = i; j >= 0; j--) {
-                cur[j] = Math.min(front[j], front[j + 1]) + triangle.get(i).get(j);
-            }
-            front = cur;
-        }
-        return front[0];
+        dp = new int[triangle.size()][triangle.size()];
+        for (int i[] : dp)
+            Arrays.fill(i, -1);
+        return helper(triangle, 0, 0);
     }
 
-    public int helper(List<List<Integer>> al, int i, int j) {
-        if (i >= al.size() || j >= al.get(i).size())
+    public int helper(List<List<Integer>> triangle, int i, int j) {
+        if (i == triangle.size())
             return 0;
         if (dp[i][j] != -1)
             return dp[i][j];
-        int one = (int) 1e7;
-        int two = (int) 1e7;
-        one = helper(al, i + 1, j) + al.get(i).get(j);
-        two = helper(al, i + 1, j + 1) + al.get(i).get(j);
-        return dp[i][j] = Math.min(one, two);
+        return dp[i][j] = triangle.get(i).get(j) + Math.min(helper(triangle, i + 1, j), helper(triangle, i + 1, j + 1));
     }
 }
