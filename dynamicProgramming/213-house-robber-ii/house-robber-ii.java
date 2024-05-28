43281bd8bdaff4de29697983a5184b5364122add
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-28 22:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (71.49%) - LeetSync

--- dynamicProgramming/213-house-robber-ii/house-robber-ii.java
+++ dynamicProgramming/213-house-robber-ii/house-robber-ii.java
@@ -1,19 +1,25 @@
 class Solution {
+
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1)
+        if(nums.length == 1)
             return nums[0];
-        return Math.max(helper(nums, 0, n - 2), helper(nums, 1, n - 1));
+        int[] dp = new int[nums.length];
+        int[] dp1 = new int[nums.length];
+        Arrays.fill(dp, -1);
+        Arrays.fill(dp1, -1);
+        int n = nums.length;
+        return Math.max(helper(nums, n - 2, 0, dp), helper(nums, n - 1, 1, dp1));
     }
 
-    public int helper(int nums[], int start, int end) {
-        int prev = nums[start];
-        int prev1 = 0;
-        for (int i = start + 1; i <= end; i++) {
-            int cur = Math.max(prev, prev1 + nums[i]);
-            prev1 = prev;
-            prev = cur;
-        }
-        return prev;
+    public int helper(int nums[], int idx, int end, int[] dp) {
+        if (idx < 0)
+            return 0;
+        if (idx < end)
+            return 0;
+        if (dp[idx] != -1)
+            return dp[idx];
+        int take = nums[idx] + helper(nums, idx - 2, end, dp);
+        int notTake = helper(nums, idx - 1, end, dp);
+        return dp[idx] = Math.max(take, notTake);
     }
 }
