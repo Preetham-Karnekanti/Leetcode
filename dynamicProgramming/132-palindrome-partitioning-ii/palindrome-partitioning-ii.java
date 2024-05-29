031bebffa6b807a3e6b88668d912aefd946d439b
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-29 10:00:23 +0530

    Time: 2541 ms (14.01%) | Memory: 41.3 MB (72.61%) - LeetSync

--- dynamicProgramming/132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ dynamicProgramming/132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -2,9 +2,18 @@ class Solution {
     int dp[];
 
     public int minCut(String s) {
-        dp = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return helper(s, 0) - 1;
+        dp = new int[s.length() + 1];
+        for (int idx = s.length() - 1; idx >= 0; idx--) {
+            int min = (int) 1e6;
+            for (int i = idx; i < s.length(); i++) {
+                if (isPallindrome(s, idx, i)) {
+                    int ans = 1 + dp[i + 1];
+                    min = Math.min(min, ans);
+                }
+                dp[idx] = min;
+            }
+        }
+        return dp[0] - 1;
     }
 
     public int helper(String s, int idx) {
