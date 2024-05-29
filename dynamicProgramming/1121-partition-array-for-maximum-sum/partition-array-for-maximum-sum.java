193a32fb1ab6b371b3e633658f0412ebb8abb54b
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-29 10:53:11 +0530

    Time: 6 ms (93.26%) | Memory: 41.8 MB (82.12%) - LeetSync

--- dynamicProgramming/1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ dynamicProgramming/1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -2,9 +2,16 @@ class Solution {
     int dp[];
 
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        dp = new int[arr.length];
-        Arrays.fill(dp, -1);
-        return helper(arr, k, 0);
+        dp = new int[arr.length + 1];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int max = 0, res = 0;
+            for (int j = i; j < arr.length && j < i + k; j++) {
+                max = Math.max(max, arr[j]);
+                res = Math.max(res, (j - i + 1) * max + dp[j + 1]);
+            }
+            dp[i] = res;
+        }
+        return dp[0];
     }
 
     public int helper(int[] arr, int k, int i) {
