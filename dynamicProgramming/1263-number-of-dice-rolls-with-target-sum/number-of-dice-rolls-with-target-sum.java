0e16df7171f3d240b9a575d7fe667041cadca7f6
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-30 21:52:13 +0530

    Time: 13 ms (48.27%) | Memory: 41.8 MB (64.24%) - LeetSync

--- dynamicProgramming/1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ dynamicProgramming/1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,37 +1,38 @@
 class Solution {
     int m = (int) 1e9 + 7;
-    int dp[][];
 
     public int numRollsToTarget(int n, int k, int target) {
-        dp = new int[n + 1][target + 1];
-        dp[0][0] = 1;
+        int prev[] = new int[target + 1];
+        prev[0] = 1;
         for (int i = 1; i <= n; i++) {
+            int cur[] = new int[target+1];
             for (int j = 0; j <= target; j++) {
                 int ans = 0;
                 for (int idx = 1; idx <= k; idx++) {
                     if (idx <= j) {
-                        ans = (ans + dp[i - 1][j - idx]) % m;
+                        ans = (ans + prev[j - idx]) % m;
                     }
                 }
-                dp[i][j] = ans;
+                cur[j] = ans;
             }
+            prev = cur;
         }
-        return dp[n][target];
+        return prev[target];
     }
 
-    public int helper(int n, int k, int target) {
-        if (target == 0 && n == 0)
-            return 1;
-        if (n == 0)
-            return 0;
-        if (dp[n][target] != -1)
-            return dp[n][target];
-        int ans = 0;
-        for (int idx = 1; idx <= k; idx++) {
-            if (idx <= target) {
-                ans = (ans + helper(n - 1, k, target - idx)) % m;
-            }
-        }
-        return dp[n][target] = ans;
-    }
+    // public int helper(int n, int k, int target) {
+    // if (target == 0 && n == 0)
+    // return 1;
+    // if (n == 0)
+    // return 0;
+    // if (dp[n][target] != -1)
+    // return dp[n][target];
+    // int ans = 0;
+    // for (int idx = 1; idx <= k; idx++) {
+    // if (idx <= target) {
+    // ans = (ans + helper(n - 1, k, target - idx)) % m;
+    // }
+    // }
+    // return dp[n][target] = ans;
+    // }
 }
