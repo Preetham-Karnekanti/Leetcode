bab3d6e666fa307660b9d88daf1ee7eefe18986d
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-30 21:50:07 +0530

    Time: 14 ms (44.44%) | Memory: 41.7 MB (64.24%) - LeetSync

--- dynamicProgramming/1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ dynamicProgramming/1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -4,9 +4,19 @@ class Solution {
 
     public int numRollsToTarget(int n, int k, int target) {
         dp = new int[n + 1][target + 1];
-        for (int i[] : dp)
-            Arrays.fill(i, -1);
-        return helper(n, k, target);
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= target; j++) {
+                int ans = 0;
+                for (int idx = 1; idx <= k; idx++) {
+                    if (idx <= j) {
+                        ans = (ans + dp[i - 1][j - idx]) % m;
+                    }
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[n][target];
     }
 
     public int helper(int n, int k, int target) {
@@ -17,9 +27,9 @@ class Solution {
         if (dp[n][target] != -1)
             return dp[n][target];
         int ans = 0;
-        for (int i = 1; i <= k; i++) {
-            if (i <= target) {
-                ans = (ans  + helper(n - 1, k, target - i)) % m;
+        for (int idx = 1; idx <= k; idx++) {
+            if (idx <= target) {
+                ans = (ans + helper(n - 1, k, target - idx)) % m;
             }
         }
         return dp[n][target] = ans;
