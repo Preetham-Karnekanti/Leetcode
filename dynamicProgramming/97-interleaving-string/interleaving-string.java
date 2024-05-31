39c1575adcff998755371d4f62ee6f5f5dddafe6
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-31 11:46:33 +0530

    Time: 7 ms (27.16%) | Memory: 41.1 MB (96.58%) - LeetSync

--- dynamicProgramming/97-interleaving-string/interleaving-string.java
+++ dynamicProgramming/97-interleaving-string/interleaving-string.java
@@ -1,27 +1,43 @@
 class Solution {
-    int dp[][];
-
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length())
+        int x = s1.length();
+        int y = s2.length();
+        int z = s3.length();
+        if (x + y != z) {
             return false;
-        dp = new int[s1.length() + 1][s2.length() + 1];
-        for (int i[] : dp)
-            Arrays.fill(i, -1);
-        return helper(s1, s2, s3, 0, 0);
-    }
+        }
+        // Table Creation
+        boolean dp[][] = new boolean[x + 1][y + 1];
+        dp[0][0] = true;
 
-    public boolean helper(String s1, String s2, String s3, int i, int j) {
-        if (i + j == s3.length())
-            return true;
-        if (dp[i][j] != -1)
-            return dp[i][j] == 0 ? false : true;
-        boolean one = false;
-        boolean two = false;
-        if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j))
-            one = helper(s1, s2, s3, i + 1, j);
-        if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j))
-            two = helper(s1, s2, s3, i, j + 1);
-        dp[i][j] = (two || one) == true ? 1 : 0;
-        return two || one;
+        // Base Case Initialization
+        for (int i = 1; i <= x; i++) {
+            if (s1.charAt(i - 1) == s3.charAt(i - 1)) {
+                dp[i][0] = true;
+            } else
+                break;
+        }
+
+        for (int j = 1; j <= y; j++) {
+            if (s2.charAt(j - 1) == s3.charAt(j - 1)) {
+                dp[0][j] = true;
+            } else
+                break;
+        }
+
+        // Fill - Bottom Up
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
+                if (s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                if (!dp[i][j] && s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = dp[i][j - 1];
+                }
+                // !dp[i][j] : so that i will go for 2nd condition only if 1st if statement
+                // doesn't satisfy the conditons.
+            }
+        }
+        return dp[x][y];
     }
 }
