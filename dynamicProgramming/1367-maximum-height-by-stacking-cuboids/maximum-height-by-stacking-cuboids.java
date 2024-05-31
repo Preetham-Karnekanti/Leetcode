1c1f81c3900f0e6c7f57349463bdc097fa6e3587
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-05-31 12:50:45 +0530

    Time: 23 ms (7.91%) | Memory: 43.6 MB (18.08%) - LeetSync

--- dynamicProgramming/1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.java
+++ dynamicProgramming/1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.java
@@ -1,11 +1,16 @@
 class Solution {
+    int dp[][];
+
     public int maxHeight(int[][] cuboids) {
+        dp = new int[cuboids.length][cuboids.length + 1];
+        for (int i[] : dp)
+            Arrays.fill(i, -1);
         for (int i = 0; i < cuboids.length; i++)
             Arrays.sort(cuboids[i]);
         Arrays.sort(cuboids, (a, b) -> {
             return b[0] * b[1] * b[2] - a[0] * a[1] * a[2];
         });
-        for(int i = 0;i<cuboids.length;i++)
+        for (int i = 0; i < cuboids.length; i++)
             System.out.println(Arrays.toString(cuboids[i]));
         return helper(cuboids, 0, -1);
     }
@@ -13,12 +18,14 @@ class Solution {
     public int helper(int[][] cuboids, int idx, int prev) {
         if (idx == cuboids.length)
             return 0;
+        if (dp[idx][prev + 1] != -1)
+            return dp[idx][prev + 1];
         int notTake = helper(cuboids, idx + 1, prev);
         int take = 0;
         if (prev == -1 || check(cuboids[prev], cuboids[idx])) {
             take = cuboids[idx][2] + helper(cuboids, idx + 1, idx);
         }
-        return Math.max(take, notTake);
+        return dp[idx][prev + 1] = Math.max(take, notTake);
     }
 
     public boolean check(int[] a, int b[]) {
