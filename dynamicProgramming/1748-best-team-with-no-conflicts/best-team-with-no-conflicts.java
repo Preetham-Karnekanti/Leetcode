d8d26a04c7bb3d2f9ca501445a90b353102f3c70
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-02 16:28:24 +0530

    Time: 31 ms (74.63%) | Memory: 44.8 MB (66.79%) - LeetSync

--- dynamicProgramming/1748-best-team-with-no-conflicts/best-team-with-no-conflicts.java
+++ dynamicProgramming/1748-best-team-with-no-conflicts/best-team-with-no-conflicts.java
@@ -1,28 +1,38 @@
 class Solution {
-    Integer dp[][];
 
     public int bestTeamScore(int[] scores, int[] ages) {
         int n = scores.length;
-        dp = new Integer[n][n + 1];
         int arr[][] = new int[n][2];
         for (int i = 0; i < n; i++) {
             arr[i][0] = ages[i];
             arr[i][1] = scores[i];
         }
         Arrays.sort(arr, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-        return helper(arr, 0, -1);
+
+        int ans = 1;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; ++i) {
+            dp[i] = arr[i][1];
+            for (int j = 0; j < i; ++j) {
+                if (arr[j][1] <= arr[i][1]) {
+                    dp[i] = Math.max(dp[i], arr[i][1] + dp[j]);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
     }
 
-    public int helper(int arr[][], int idx, int prev) {
-        if (idx == arr.length)
-            return 0;
-        if (dp[idx][prev + 1] != null)
-            return dp[idx][prev + 1];
-        int notTake = helper(arr, idx + 1, prev);
-        int take = 0;
-        if (prev == -1 || arr[idx][1] >= arr[prev][1]) {
-            take = arr[idx][1] + helper(arr, idx + 1, idx);
-        }
-        return dp[idx][prev + 1] = Math.max(take, notTake);
-    }
+    // public int helper(int arr[][], int idx, int prev) {
+    //     if (idx == arr.length)
+    //         return 0;
+    //     if (dp[idx][prev + 1] != null)
+    //         return dp[idx][prev + 1];
+    //     int notTake = helper(arr, idx + 1, prev);
+    //     int take = 0;
+    //     if (prev == -1 || arr[idx][1] >= arr[prev][1]) {
+    //         take = arr[idx][1] + helper(arr, idx + 1, idx);
+    //     }
+    //     return dp[idx][prev + 1] = Math.max(take, notTake);
+    // }
 }
