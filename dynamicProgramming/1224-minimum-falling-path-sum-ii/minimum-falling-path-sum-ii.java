9b4ed924830e3e7ffe76475d779b8ac33db97b66
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-03 11:53:15 +0530

    Time: 38 ms (37.27%) | Memory: 51.4 MB (30.23%) - LeetSync

--- dynamicProgramming/1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ dynamicProgramming/1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -1,28 +1,40 @@
 class Solution {
-    Integer dp[][];
 
     public int minFallingPathSum(int[][] grid) {
         int min = Integer.MAX_VALUE;
-        dp = new Integer[grid.length][grid[0].length];
+        int[][] dp = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid[0].length; i++)
+            dp[0][i] = grid[0][i];
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                int res = (int) 1e8;
+                for (int k = 0; k < grid[0].length; k++) {
+                    if (k != j) {
+                        res = Math.min(res, grid[i][j] + dp[i - 1][k]);
+                    }
+                }
+                dp[i][j] = res;
+            }
+        }
         for (int i = 0; i < grid[0].length; i++) {
-            min = Math.min(min, helper(grid, 0, i));
+            min = Math.min(dp[grid.length - 1][i], min);
         }
         return min;
     }
 
-    public int helper(int grid[][], int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
-            return 0;
-        if (i == grid.length - 1)
-            return grid[i][j];
-        if (dp[i][j] != null)
-            return dp[i][j];
-        int res = (int) 1e8;
-        for (int k = 0; k < grid[0].length; k++) {
-            if (k != j) {
-                res = Math.min(res, grid[i][j] + helper(grid, i + 1, k));
-            }
-        }
-        return dp[i][j] = res;
-    }
+    // public int helper(int grid[][], int i, int j) {
+    // if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
+    // return 0;
+    // if (i == grid.length - 1)
+    // return grid[i][j];
+    // if (dp[i][j] != null)
+    // return dp[i][j];
+    // int res = (int) 1e8;
+    // for (int k = 0; k < grid[0].length; k++) {
+    // if (k != j) {
+    // res = Math.min(res, grid[i][j] + helper(grid, i + 1, k));
+    // }
+    // }
+    // return dp[i][j] = res;
+    // }
 }
