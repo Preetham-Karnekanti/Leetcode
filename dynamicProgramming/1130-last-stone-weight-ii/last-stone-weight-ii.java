92bd15f8d89c24cb56d48ec3040d00ff16e5b66f
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-03 23:20:23 +0530

    Time: 7 ms (13.40%) | Memory: 44.8 MB (8.93%) - LeetSync

--- dynamicProgramming/1130-last-stone-weight-ii/last-stone-weight-ii.java
+++ dynamicProgramming/1130-last-stone-weight-ii/last-stone-weight-ii.java
@@ -5,16 +5,16 @@ class Solution {
         dp = new int[31][3001];
         for (int i[] : dp)
             Arrays.fill(i, -1);
-        return helper(stones, 0, 0, 0);
+        return helper(stones, 0, 0);
     }
 
-    public int helper(int nums[], int idx, int left, int right) {
+    public int helper(int nums[], int idx, int left) {
         if (idx >= nums.length)
-            return Math.abs(left - right);
+            return left;
         if (dp[idx][left] != -1)
             return dp[idx][left];
-        int a = helper(nums, idx + 1, left + nums[idx], right);
-        int b = helper(nums, idx + 1, left, right + nums[idx]);
+        int a = helper(nums, idx + 1, left + nums[idx]);
+        int b = helper(nums, idx + 1, Math.abs(left - nums[idx]));
         return dp[idx][left] = Math.min(a, b);
     }
 }
