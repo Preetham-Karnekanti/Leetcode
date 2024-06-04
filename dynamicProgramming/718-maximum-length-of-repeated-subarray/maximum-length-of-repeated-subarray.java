efbce78ebf10350de8fdbe9f3ffad1a341f14356
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-04 11:02:59 +0530

    Time: 24 ms (56.65%) | Memory: 44.4 MB (86.92%) - LeetSync

--- dynamicProgramming/718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
+++ dynamicProgramming/718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
@@ -1,27 +1,19 @@
 class Solution {
-    private int lcs(int n1[], int n2[], int i, int j, int ans[], int dp[][]) {
-        if (i >= n1.length || j >= n2.length)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int cur = 0;
-        if (n1[i] == n2[j]) {
-            cur = lcs(n1, n2, i + 1, j + 1, ans, dp) + 1;
-        }
-        lcs(n1, n2, i, j + 1, ans, dp);
-        lcs(n1, n2, i + 1, j, ans, dp);
-        ans[0] = Math.max(ans[0], cur);
-        return dp[i][j] = cur;
-    }
-
     public int findLength(int[] nums1, int[] nums2) {
         int n = nums1.length, m = nums2.length;
-        int dp[][] = new int[n + 1][m + 1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        int prev[] = new int[m + 1];
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int cur[] = new int[m + 1];
+            for (int j = 1; j <= m; j++) {
+                if (nums1[i - 1] == nums2[j - 1])
+                    cur[j] = prev[j - 1] + 1;
+                else
+                    cur[j] = 0;
+                ans = Math.max(ans, cur[j]);
+            }
+            prev = cur;
         }
-        int ans[] = new int[1];
-        lcs(nums1, nums2, 0, 0, ans, dp);
-        return ans[0];
+        return ans;
     }
 }
