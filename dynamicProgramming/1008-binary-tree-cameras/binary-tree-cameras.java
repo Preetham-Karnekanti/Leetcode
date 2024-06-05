cdbe0b9e9f2cba2837f8d7b07e1d887606a05f19
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-05 14:32:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (21.08%) - LeetSync

---
+++ dynamicProgramming/1008-binary-tree-cameras/binary-tree-cameras.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    int I_NEED_CAMERA = -1;
+    int I_AM_COVERED = 1;
+    int I_HAVE_CAMERA = 0;
+    int camera = 0;
+
+    public int minCameraCover(TreeNode root) {
+        camera = 0;
+        int ans = helper(root);
+        if (ans == -1)
+            camera++;
+        return camera;
+    }
+
+    public int helper(TreeNode root) {
+        if (root == null)
+            return I_AM_COVERED;
+        int lchild = helper(root.left); // take information from left child
+        int rchild = helper(root.right); // take information from right child
+        if (lchild == I_NEED_CAMERA || rchild == I_NEED_CAMERA) {
+            camera++;
+            return I_HAVE_CAMERA;
+        }
+        if (lchild == I_HAVE_CAMERA || rchild == I_HAVE_CAMERA)
+            return I_AM_COVERED;
+        return I_NEED_CAMERA;
+    }
+}
