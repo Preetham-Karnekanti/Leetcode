b3843ee44098397b5466101267222a03b4b54c7c
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-06 10:55:48 +0530

    Time: 8 ms (100.00%) | Memory: 44.9 MB (93.23%) - LeetSync

--- dynamicProgramming/876-hand-of-straights/hand-of-straights.java
+++ dynamicProgramming/876-hand-of-straights/hand-of-straights.java
@@ -1,17 +1,33 @@
 class Solution {
+    public boolean findsucessors(int[] hand, int groupSize, int i, int n) {
+        int f = hand[i] + 1;
+        hand[i] = -1;
+        int count = 1;
+        i += 1;
+        while (i < n && count < groupSize) {
+            if (hand[i] == f) {
+                f = hand[i] + 1;
+                hand[i] = -1;
+                count++;
+            }
+            i++;
+        }
+        if (count != groupSize)
+            return false;
+        else
+            return true;
+    }
+
     public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+        if (n % groupSize != 0)
+            return false;
         Arrays.sort(hand);
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int i : hand)
-            hm.put(i, hm.getOrDefault(i, 0) + 1);
-        for (int i = 0; i < hand.length; i++) {
-            if (hm.get(hand[i]) == 0)
-                continue;
-            int start = hand[i];
-            for (int j = start; j < start + groupSize; j++) {
-                if (hm.get(j) == null || hm.get(j) <= 0)
+        int i = 0;
+        for (; i < n; i++) {
+            if (hand[i] >= 0) {
+                if (!findsucessors(hand, groupSize, i, n))
                     return false;
-                hm.put(j, hm.get(j) - 1);
             }
         }
         return true;
