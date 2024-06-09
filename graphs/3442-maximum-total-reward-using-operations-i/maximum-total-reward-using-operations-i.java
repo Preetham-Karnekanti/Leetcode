f0200275ead9aae158a74e8317d494f1df26e5f8
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-09 14:57:51 +0530

    Time: 132 ms (100.00%) | Memory: 68.8 MB (100.00%) - LeetSync

--- graphs/3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.java
+++ graphs/3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.java
@@ -1,22 +1,22 @@
 class Solution {
-    Integer dp[][];
+    Integer dp[];
     public int maxTotalReward(int[] rewards) {
         int sum = 0;
         for(int i : rewards)
             sum+=i;
-        dp = new Integer[rewards.length][4000];
+        dp = new Integer[sum];
         Arrays.sort(rewards);
         return helper(rewards, 0, 0);
     }
     public int helper(int[] rewards, int idx, int x){
         if(idx == rewards.length)
             return 0;
-        if(dp[idx][x]!=null)
-            return dp[idx][x];
+        if(dp[x]!=null)
+            return dp[x];
         int notTake = helper(rewards, idx+1, x);
         int take = 0;
         if(rewards[idx] > x)
             take = rewards[idx] + helper(rewards, idx+1, x + rewards[idx]);
-        return  dp[idx][x] = Math.max(take,notTake);
+        return  dp[x] = Math.max(take,notTake);
     }
 }
