ef77d5283610bf86a4930437636af48c35963faa
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-09 19:54:11 +0530

    Time: 55 ms (44.36%) | Memory: 44.7 MB (90.97%) - LeetSync

---
+++ graphs/1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int dx[] = new int[] { -1, 1, 0, 0 };
+        int dy[] = new int[] { 0, 0, -1, 1 };
+        int rows = heights.length, cols = heights[0].length;
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        int[][] dist = new int[rows][cols];
+        for (int i[] : dist)
+            Arrays.fill(i, Integer.MAX_VALUE);
+        dist[0][0] = 0;
+        q.add(new int[] { 0, 0, 0 });
+        int min = Integer.MAX_VALUE;
+        while (!q.isEmpty()) {
+            int cur[] = q.poll();
+            int distance = cur[0];
+            int r = cur[1];
+            int c = cur[2];
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+                if (!isValid(nr, nc, rows, cols))
+                    continue;
+                int newEffort = Math.max(Math.abs(heights[r][c] - heights[nr][nc]), distance);
+                if (newEffort < dist[nr][nc]) {
+                    dist[nr][nc] = newEffort;
+                    q.add(new int[] { newEffort, nr, nc });
+                }
+            }
+        }
+
+        return dist[rows - 1][cols - 1];
+    }
+
+    public boolean isValid(int r, int c, int rows, int cols) {
+        if (r < 0 || c < 0 || c >= cols || r >= rows)
+            return false;
+        return true;
+    }
+}
