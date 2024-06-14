a3819b858504601e24e8493991bb633bc85a7691
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-14 12:21:31 +0530

    Time: 28 ms (21.35%) | Memory: 43 MB (5.32%) - LeetSync

--- graphs/133-clone-graph/clone-graph.java
+++ graphs/133-clone-graph/clone-graph.java
@@ -1,46 +1,29 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
 
 class Solution {
     public Node cloneGraph(Node node) {
-        if (node == null || node.neighbors == null)
-            return node;
-        HashMap<Node, Node> hm = new HashMap<>();
-        Queue<Node> q = new LinkedList<>();
-        q.add(node);
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            Node newNode = new Node(cur.val);
-            hm.put(cur, newNode);
-            for (Node n : cur.neighbors) {
-                if (!hm.containsKey(n))
-                    q.add(n);
+        if (node == null) {
+            return null;
+        }
+
+        HashMap<Node, Node> visited = new HashMap<>();
+        Queue<Node> queue = new LinkedList<>();
+
+        visited.put(node, new Node(node.val));
+
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            Node currNode = queue.poll();
+
+            for (Node neighbor : currNode.neighbors) {
+                if (!visited.containsKey(neighbor)) {
+                    visited.put(neighbor, new Node(neighbor.val));
+                    queue.offer(neighbor);
+                }
+                visited.get(currNode).neighbors.add(visited.get(neighbor));
             }
         }
-        for (Node key : hm.keySet()) {
-            Node cloned = hm.get(key);
-            for (Node n : key.neighbors) {
-                Node clonedNeighbor = hm.get(n);
-                cloned.neighbors.add(clonedNeighbor);
-            }
-        }
-        return hm.get(node);
+
+        return visited.get(node);
     }
 }
