0f0a300202ceff44c03c9f5f662b0b248be05c44
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-19 09:12:28 +0530

    Time: 27 ms (11.31%) | Memory: 57.4 MB (72.15%) - LeetSync

---
+++ graphs/1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        int low = Integer.MAX_VALUE;
+        int high = 0;
+        int ans = -1;
+        for (int i : bloomDay) {
+            high = Math.max(high, i);
+            low = Math.min(low, i);
+        }
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (isPossibleWithinMidDays(bloomDay, mid, m, k)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isPossibleWithinMidDays(int[] nums, int days, int m, int k) {
+        int bouquetsMadeTillNow = 0;
+        int adjacentCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= days) {
+                adjacentCount++;
+            } else {
+                adjacentCount = 0;
+            }
+            if (adjacentCount == k) {
+                bouquetsMadeTillNow++;
+                adjacentCount = 0;
+            }
+        }
+        return bouquetsMadeTillNow >= m;
+    }
+}
