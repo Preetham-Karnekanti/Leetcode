351820d42cc5d34a72503da8576543417b33035b
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-06-21 22:35:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) 
+    { 
+        //code here.
+        int carry = 1;
+        Node reversed = reverse(head);
+        Node temp = reversed;
+        while(reversed != null && carry == 1){
+            int sum = reversed.data + carry;
+            reversed.data = sum%10;
+            carry = sum/10;
+            reversed = reversed.next;
+        }
+        Node ans = reverse(temp);
+        if(carry != 0){
+            Node ret = new Node(carry);
+            ret.next = ans;
+            ans = ret;
+        }
+        return ans;
+    }
+    
+    public static Node reverse(Node head){
+        Node prev = null, cur = head;
+        while(cur != null){
+            Node next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
