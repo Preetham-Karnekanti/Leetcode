e0b811e45488956abedb1a45e4d8b4772625c9a0
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-07-01 21:33:28 +0530

    Time: 41 ms (94.13%) | Memory: 112.7 MB (75.56%) - LeetSync

---
+++ tries/146-lru-cache/lru-cache.java
@@ -0,0 +1,118 @@
+class LRUCache {
+
+    int size;
+    HashMap<Integer, Node> hm;
+    DLL dll;
+
+    public LRUCache(int capacity) {
+        size = capacity;
+        hm = new HashMap<>();
+        dll = new DLL();
+    }
+
+    public int get(int key) {
+        if (hm.get(key) == null)
+            return -1;
+        Node node = hm.get(key);
+        dll.moveNodeToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (hm.get(key) == null) {
+            Node newNode = new Node(key, value);
+            hm.put(key, newNode);
+            dll.addFirst(newNode);
+            if (hm.size() > size) {
+                Node deleted = dll.removeTail();
+                hm.remove(deleted.key);
+            }
+        } else {
+            Node node = hm.get(key);
+            node.value = value;
+            dll.moveNodeToHead(node);
+        }
+    }
+
+    public void print(DLL dll) {
+        Node temp = dll.head;
+        while (temp != null) {
+            System.out.print(temp.value + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+
+class Node {
+    int value, key;
+    Node next, prev;
+
+    Node(int key, int data) {
+        this.key = key;
+        this.value = data;
+        next = prev = null;
+    }
+}
+
+class DLL {
+    Node head, tail;
+
+    DLL() {
+        tail = head = null;
+    }
+
+    public void addFirst(Node node) {
+        if (head == null && tail == null) {
+            head = tail = node;
+            return;
+        }
+        head.prev = node;
+        node.next = head;
+        head = node;
+    }
+
+    public void moveNodeToHead(Node node) {
+        if (head == node)
+            return;
+        if (node == tail) {
+            Node prev = node.prev;
+            prev.next = null;
+            node.prev = null;
+            node.next = head;
+            head.prev = node;
+            head = node;
+            tail = prev;
+            return;
+        }
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+        node.next = head;
+        head.prev = node;
+        head = node;
+    }
+
+    public Node removeTail() {
+        Node deletedTail = tail;
+        tail = tail.prev;
+        tail.next = null;
+        return deletedTail;
+    }
+
+    public void print(Node temp) {
+        while (temp != null) {
+            System.out.print(temp.value + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
