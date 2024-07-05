708d682c49b5dec2ca8ca40d0b531993c5d6140c
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-07-05 21:48:19 +0530

    Time: 552 ms (16.03%) | Memory: 45.2 MB (16.75%) - LeetSync

---
+++ tries/976-minimum-area-rectangle/minimum-area-rectangle.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minAreaRect(int[][] points) {
+        HashSet<String> hs = new HashSet<>();
+        for (int p[] : points) {
+            String str = p[0] + ":" + p[1];
+            hs.add(str);
+        }
+        int n = points.length;
+        int area = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                if (x1 != x2 && y1 != y2) {
+                    String s1 = x1 + ":" + y2;
+                    String s2 = x2 + ":" + y1;
+                    if (hs.contains(s1) && hs.contains(s2)) {
+                        area = Math.min(area, Math.abs((x1 - x2) * (y1 - y2)));
+                    }
+                }
+            }
+        }
+        return area == Integer.MAX_VALUE ? 0 : area;
+    }
+}
