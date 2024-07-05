5e645b3dd0916c2b24a96c49adf1c4a5c7fd5472
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-07-05 12:07:05 +0530

    Time: 1 ms (99.52%) | Memory: 43.8 MB (69.50%) - LeetSync

---
+++ tries/120-triangle/triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int dp[][] = new int[triangle.size()][triangle.size()];
+        for (int i = 0; i < dp.length; i++)
+            Arrays.fill(dp[i], -1);
+        return helper(triangle, 0, 0, dp);
+    }
+
+    public static int helper(List<List<Integer>> input, int i, int j, int[][] dp) {
+        if (i >= input.size() || j >= input.get(i).size())
+            return (int) 1e9;
+        if (i == input.size() - 1)
+            return input.get(i).get(j);
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = input.get(i).get(j) + Math.min(helper(input, i + 1, j, dp), helper(input, i + 1, j + 1, dp));
+    }
+}
