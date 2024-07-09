6655a1ce931ae03f5775d01966bc2c46c684a36b
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-07-09 19:03:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum sum of Non-adjacent nodes/maximum-sum-of-nonadjacent-nodes.java
@@ -0,0 +1,140 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node left, right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class MaxSum
+{
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution ob = new Solution();
+			    int ans = ob.getMaxSum(root);
+			    System.out.println(ans);
+                t--;
+	        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*class Node
+{
+    int data;
+    Node left, right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}*/
+
+class Solution
+{
+    //Function to return the maximum sum of non-adjacent nodes.
+    static int getMaxSum(Node root)
+    {
+        // add your code here
+        HashMap<Node, Integer> hm = new HashMap<>();
+        return helper(root, hm);
+    }
+    
+    public static int helper(Node root, HashMap<Node, Integer> hm){
+        if(root == null)
+            return 0;
+        if(hm.containsKey(root))
+            return hm.get(root);
+        int pick = root.data + (root.left != null ? (helper(root.left.left, hm) + helper(root.left.right, hm)) : 0) 
+                    + (root.right != null ? (helper(root.right.left, hm) + helper(root.right.right, hm)) : 0);
+        int skip = helper(root.left, hm) + helper(root.right, hm);
+        hm.put(root, Math.max(pick, skip));
+        return Math.max(pick, skip);
+    }
+}
