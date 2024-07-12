04e6c716c43e90a91af5c42a766f4e3e2d2cc98c
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-07-12 18:26:09 +0530

    Time: 53 ms (60.13%) | Memory: 50.6 MB (31.56%) - LeetSync

---
+++ googleTagged/384-shuffle-an-array/shuffle-an-array.java
@@ -0,0 +1,44 @@
+class Solution {
+    int data[];
+    Random rand;
+
+    public Solution(int[] nums) {
+        data = new int[nums.length];
+        rand = new Random();
+        for (int i = 0; i < nums.length; i++)
+            data[i] = nums[i];
+    }
+
+    public int[] reset() {
+        return data;
+    }
+
+    int getRandomNumber() {
+        return rand.nextInt(data.length);
+    }
+
+    public int[] shuffle() {
+
+        int[] ans = new int[data.length];
+        boolean added[] = new boolean[data.length];
+
+        for (int i = 0; i < ans.length; i++) {
+            int index = getRandomNumber();
+            if (!added[index]) {
+                added[index] = true;
+                ans[i] = data[index];
+            } else
+                i--;
+
+        }
+        return ans;
+        // return data;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
