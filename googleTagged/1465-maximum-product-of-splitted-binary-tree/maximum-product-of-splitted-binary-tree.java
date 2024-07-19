e9e2d674f8a61fc03fa0d0d6196b0380296db387
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-07-19 19:36:02 +0530

    Time: 7 ms (57.78%) | Memory: 58.6 MB (27.62%) - LeetSync

---
+++ googleTagged/1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    int m = (int) 1e9 + 7;
+
+    public int maxProduct(TreeNode root) {
+        long totalSum = findTreeSum(root);
+        long max[] = new long[1];
+        findMaxProduct(root, totalSum, max);
+        return (int) (max[0] % m);
+    }
+
+    public long findTreeSum(TreeNode root) {
+        if (root == null)
+            return 0;
+        long left = findTreeSum(root.left);
+        long right = findTreeSum(root.right);
+        return left + right + root.val;
+    }
+
+    public long findMaxProduct(TreeNode root, long totalSum, long max[]) {
+        if (root == null)
+            return 0;
+        long left = findMaxProduct(root.left, totalSum, max);
+        long right = findMaxProduct(root.right, totalSum, max);
+        long sum = root.val + left + right;
+        max[0] = Math.max(max[0], sum * (totalSum - sum));
+        return sum;
+    }
+}
