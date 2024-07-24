40a1d5401f290e1f53ea90e11de4f6395c38edd1
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-07-24 09:50:18 +0530

    Time: 492 ms (11.76%) | Memory: 55.4 MB (59.80%) - LeetSync

--- googleTagged/1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
+++ googleTagged/1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -2,18 +2,26 @@ class Solution {
     public int[] sortJumbled(int[] mapping, int[] nums) {
         int n = nums.length;
         Pair numWithIdx[] = new Pair[n];
+        HashMap<Integer, Integer> hm = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             numWithIdx[i] = new Pair(nums[i], i);
         }
         Arrays.sort(numWithIdx, (a, b) -> {
-            int aNum = convertToMappings(a.value, mapping);
-            int bNum = convertToMappings(b.value, mapping);
+            int aNum = getMapping(a.value, mapping, hm);
+            int bNum = getMapping(b.value, mapping, hm);
             return aNum == bNum ? a.idx - b.idx : aNum - bNum;
         });
         for (int i = 0; i < n; i++) {
             nums[i] = numWithIdx[i].value;
         }
         return nums;
+    }
+    public int getMapping(int num, int mappings[], HashMap<Integer, Integer> hm){
+        if(hm.containsKey(num))
+            return hm.get(num);
+        int mappedNumber = convertToMappings(num, mappings);
+        hm.put(num, mappedNumber);
+        return mappedNumber;
     }
 
     public int convertToMappings(int num, int[] mappings) {
