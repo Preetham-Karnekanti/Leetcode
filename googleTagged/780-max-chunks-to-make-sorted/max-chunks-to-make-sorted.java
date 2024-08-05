0194e9df260e4b0074144d68991172be39dc3103
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-06 00:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (17.38%) - LeetSync

--- googleTagged/780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ googleTagged/780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,19 +1,13 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
+        int max = 0;
         int count = 0;
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i = 0;i<arr.length;i++){
-            hs.add(arr[i]);
-            if(hasAll(hs, i))
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i])
+                max = arr[i];
+            if (max == i)
                 count++;
         }
         return count;
-    }
-    public boolean hasAll(HashSet<Integer> hs, int i){
-        for(int k = 0;k<=i;k++){
-            if(!hs.contains(k))
-                return false;
-        }
-        return true;
     }
 }
