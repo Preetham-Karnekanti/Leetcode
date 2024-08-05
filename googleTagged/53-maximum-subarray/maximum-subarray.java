f7a2bbe357a191762269781616cc5db2ea7c079b
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-05 23:13:41 +0530

    Time: 11 ms (6.32%) | Memory: 57 MB (52.03%) - LeetSync

--- googleTagged/53-maximum-subarray/maximum-subarray.java
+++ googleTagged/53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,26 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE;
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public int helper(int nums[], int low, int high) {
+        if (low == high)
+            return nums[low];
+        int mid = (low + high) / 2;
+        int maxLeftSum = Integer.MIN_VALUE;
         int sum = 0;
-        for(int i : nums){
-            sum +=i;
-            max = Math.max(max, sum);
-            sum = Math.max(sum, 0);
+        for (int i = mid; i >= low; i--) {
+            sum += nums[i];
+            maxLeftSum = Math.max(maxLeftSum, sum);
         }
-        return max;
+
+        int maxRightSum = Integer.MIN_VALUE;
+        sum = 0;
+        for (int i = mid + 1; i <= high; i++) {
+            sum += nums[i];
+            maxRightSum = Math.max(maxRightSum, sum);
+        }
+        int max = Math.max(helper(nums, low, mid), helper(nums, mid + 1, high));
+        return Math.max(max, maxRightSum + maxLeftSum);
     }
 }
