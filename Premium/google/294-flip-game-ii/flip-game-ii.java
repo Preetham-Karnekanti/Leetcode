4676032618ae5d9ea8ca3ac1912c452a5c6653b5
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-09 00:02:29 +0530

    Time: 13 ms (66.97%) | Memory: 44.9 MB (67.89%) - LeetSync

---
+++ Premium/google/294-flip-game-ii/flip-game-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canWin(String currentState) {
+        HashMap<String, Boolean> hm = new HashMap<>();
+        return helper(currentState, hm);
+    }
+
+    public boolean helper(String s, HashMap<String, Boolean> hm) {
+        if (s == null || s.length() < 2)
+            return false;
+        if (hm.containsKey(s))
+            return hm.get(s);
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.startsWith("++", i)) {
+                String t = s.substring(0, i) + "--" + s.substring(i + 2);
+                if (!helper(t, hm)) {
+                    hm.put(s, true);
+                    return true;
+                }
+            }
+        }
+        hm.put(s, false);
+        return false;
+    }
+}
