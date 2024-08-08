3ddbf20e4ee029dd705da496b99e4c62108b6655
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-09 00:03:43 +0530

    Time: 13 ms (66.97%) | Memory: 45.2 MB (24.77%) - LeetSync

--- Premium/google/294-flip-game-ii/flip-game-ii.java
+++ Premium/google/294-flip-game-ii/flip-game-ii.java
@@ -9,8 +9,8 @@ class Solution {
             return false;
         if (hm.containsKey(s))
             return hm.get(s);
-        for (int i = 0; i < s.length(); i++) {
-            if (s.startsWith("++", i)) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
                 String t = s.substring(0, i) + "--" + s.substring(i + 2);
                 if (!helper(t, hm)) {
                     hm.put(s, true);
