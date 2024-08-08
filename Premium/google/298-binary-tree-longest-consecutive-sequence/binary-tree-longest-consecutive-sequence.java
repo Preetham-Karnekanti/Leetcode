f9f8231fe9ce43dd047d3cf73ee30a06eb4ab58f
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-09 00:24:39 +0530

    Time: 1 ms (80.34%) | Memory: 45.6 MB (78.21%) - LeetSync

--- Premium/google/298-binary-tree-longest-consecutive-sequence/binary-tree-longest-consecutive-sequence.java
+++ Premium/google/298-binary-tree-longest-consecutive-sequence/binary-tree-longest-consecutive-sequence.java
@@ -18,23 +18,16 @@ class Solution {
         if (root == null)
             return 0;
         int max[] = new int[1];
-        helper(root, max);
+        helper(root, null, max, 0);
         return max[0];
     }
 
-    public int helper(TreeNode root, int[] max) {
+    public void helper(TreeNode root, TreeNode parent, int[] max, int length) {
         if (root == null)
-            return 0;
-        int curMax = 1;
-        int left = helper(root.left, max);
-        int right = helper(root.right, max);
-        if (root.left != null && root.val == root.left.val - 1) {
-            curMax = Math.max(curMax, 1 + left);
-        }
-        if (root.right != null && root.val == root.right.val - 1) {
-            curMax = Math.max(curMax, 1 + right);
-        }
-        max[0] = Math.max(max[0], curMax);
-        return curMax;
+            return;
+        length = (parent != null && parent.val == root.val - 1) ? length + 1 : 1;
+        max[0] = Math.max(max[0], length);
+        helper(root.left, root, max, length);
+        helper(root.right, root, max, length);
     }
 }
