3256288a7990f70c02595e2d13b39a936ece15b6
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-15 15:30:57 +0530

    Time: 313 ms (9.65%) | Memory: 45.3 MB (8.36%) - LeetSync

--- Premium/google/686-repeated-string-match/repeated-string-match.java
+++ Premium/google/686-repeated-string-match/repeated-string-match.java
@@ -1,6 +1,6 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int minRepeats = (int) Math.ceil(b.length() / a.length());
+        int minRepeats = b.length() / a.length();
         for (int i = 0; i <= minRepeats + 2; i++) {
             String repeatA = a.repeat(i);
             if (repeatA.contains(b))
