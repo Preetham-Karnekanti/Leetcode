52265df2bfe1b4dc7a0051d0b494986c9ca18329
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-15 09:47:31 +0530

    Time: 1 ms (100.00%) | Memory: 45 MB (40.21%) - LeetSync

--- Premium/google/3504-minimum-moves-to-get-a-peaceful-board/minimum-moves-to-get-a-peaceful-board.java
+++ Premium/google/3504-minimum-moves-to-get-a-peaceful-board/minimum-moves-to-get-a-peaceful-board.java
@@ -3,14 +3,19 @@ class Solution {
     public int minMoves(int[][] rooks) {
         int minMoves = 0;
 
-        Arrays.sort(rooks, (a, b) -> a[0] - b[0]);
+        int[] row = new int[rooks.length];
+        int[] col = new int[rooks.length];
         for (int i = 0; i < rooks.length; i++) {
-            minMoves += Math.abs(i - rooks[i][0]);
+            row[rooks[i][0]]++;
+            col[rooks[i][1]]++;
         }
 
-        Arrays.sort(rooks, (a, b) -> a[1] - b[1]);
+        int rowMinMoves = 0, colMinMoves = 0;
         for (int i = 0; i < rooks.length; i++) {
-            minMoves += Math.abs(i - rooks[i][1]);
+            rowMinMoves += row[i] - 1;
+            colMinMoves += col[i] - 1;
+
+            minMoves += Math.abs(rowMinMoves) + Math.abs(colMinMoves);
         }
 
         return minMoves;
