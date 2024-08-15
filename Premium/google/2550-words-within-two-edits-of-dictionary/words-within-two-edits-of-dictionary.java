6d01f67f792d2eaa668381d9f655e36f641b0d7a
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-15 16:10:57 +0530

    Time: 19 ms (19.09%) | Memory: 42.6 MB (74.09%) - LeetSync

---
+++ Premium/google/2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> ans = new ArrayList<>();
+        for (String word1 : queries) {
+            for (String word2 : dictionary) {
+                int i = 0, j = 0;
+                int count = 0;
+                while (i < word1.length() && j < word2.length()) {
+                    if (word1.charAt(i) != word2.charAt(j))
+                        count++;
+                    i++;
+                    j++;
+                }
+                if (count <= 2) {
+                    ans.add(word1);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
