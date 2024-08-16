f8cb143971465e946e7c78c74b66e30100210a6a
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-16 11:10:41 +0530

    Time: 3 ms (96.48%) | Memory: 44.1 MB (67.28%) - LeetSync

---
+++ Premium/google/1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int k = n * 5 / 100;
+        int sum = 0;
+        for (int i = k; i < n - k; i++) {
+            sum = sum + arr[i];
+        }
+        return (double) sum / (n - 2 * k);
+    }
+}
