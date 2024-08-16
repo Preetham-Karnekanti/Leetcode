0f60a3acb1b7ffedc6d70d5cd8ef71c2fb70946c
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-16 15:29:40 +0530

    Time: 5 ms (83.19%) | Memory: 50.1 MB (70.13%) - LeetSync

--- Premium/google/550-shortest-path-to-get-food/shortest-path-to-get-food.java
+++ Premium/google/550-shortest-path-to-get-food/shortest-path-to-get-food.java
@@ -7,29 +7,33 @@ class Solution {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '*') {
-                    q.add(new int[] { i, j, 0 });
+                    q.add(new int[] { i, j });
                     vis[i][j] = true;
                     break;
                 }
             }
         }
+        int steps = 0;
         int dx[] = new int[] { -1, 1, 0, 0 };
         int dy[] = new int[] { 0, 0, -1, 1 };
         while (!q.isEmpty()) {
-            int cur[] = q.poll();
-            int r = cur[0];
-            int c = cur[1];
-            int steps = cur[2];
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dx[i];
-                int nc = c + dy[i];
-                if (!isValid(nr, nc, n, m) || grid[nr][nc] == 'X' || vis[nr][nc])
-                    continue;
-                if (grid[nr][nc] == '#')
-                    return steps + 1;
-                q.add(new int[] { nr, nc, 1 + steps });
-                vis[nr][nc] = true;
+            int size = q.size();
+            while (size-- > 0) {
+                int cur[] = q.poll();
+                int r = cur[0];
+                int c = cur[1];
+                for (int i = 0; i < 4; i++) {
+                    int nr = r + dx[i];
+                    int nc = c + dy[i];
+                    if (!isValid(nr, nc, n, m) || grid[nr][nc] == 'X' || vis[nr][nc])
+                        continue;
+                    if (grid[nr][nc] == '#')
+                        return steps + 1;
+                    q.add(new int[] { nr, nc, 1 + steps });
+                    vis[nr][nc] = true;
+                }
             }
+            steps++;
         }
         return -1;
     }
