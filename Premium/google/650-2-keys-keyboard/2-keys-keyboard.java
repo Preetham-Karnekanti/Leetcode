3d1bb19e719c13ad409396f22e68152cdaaa95ef
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-19 10:05:13 +0530

    Time: 41 ms (8.04%) | Memory: 45.5 MB (20.58%) - LeetSync

--- Premium/google/650-2-keys-keyboard/2-keys-keyboard.java
+++ Premium/google/650-2-keys-keyboard/2-keys-keyboard.java
@@ -2,23 +2,22 @@ class Solution {
     HashMap<String, Integer> hm;
 
     public int minSteps(int n) {
+        if (n == 1)
+            return 0;
         hm = new HashMap<>();
-        return helper(n, 1, 0, 0);
+        return helper(n, 1, 1) + 1;
     }
 
-    public int helper(int n, int a, int copy, int pasteCount) {
+    public int helper(int n, int a, int pasteCount) {
         if (n == a)
             return 0;
         if (n < a)
             return (int) 1e9;
-        String key = a + ":" + copy + ":" + pasteCount;
+        String key = a + ":" + pasteCount;
         if (hm.get(key) != null)
             return hm.get(key);
-        if (copy == 0) {
-            return 1 + helper(n, a, 1, a);
-        }
-        int one = 1 + helper(n, a + pasteCount, 1, pasteCount);
-        int two = 1 + helper(n, a + pasteCount, 0, pasteCount);
+        int one = 1 + helper(n, a + pasteCount, pasteCount);
+        int two = 2 + helper(n, 2 * a, a);
         hm.put(key, Math.min(one, two));
         return Math.min(one, two);
     }
