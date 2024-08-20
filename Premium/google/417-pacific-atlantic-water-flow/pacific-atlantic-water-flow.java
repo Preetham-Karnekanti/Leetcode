d03a86c96a3254f9aa1c5d312235126f37b04a51
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-20 13:37:32 +0530

    Time: 11 ms (34.33%) | Memory: 45.5 MB (55.22%) - LeetSync

--- Premium/google/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ Premium/google/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,46 +1,41 @@
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int n = heights.length;
-        int m = heights[0].length;
-        boolean pacific[][] = new boolean[n][m];
+        int rows = heights.length;
+        int cols = heights[0].length;
+        boolean pacific[][] = new boolean[rows][cols];
+        boolean atlantic[][] = new boolean[rows][cols];
         Queue<int[]> q1 = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
+        Queue<int[]> q2 = new LinkedList<>();
+        for (int i = 0; i < rows; i++) {
             q1.add(new int[] { i, 0 });
             pacific[i][0] = true;
         }
-        for (int i = 0; i < m; i++) {
+        for (int i = 1; i < cols; i++) {
             pacific[0][i] = true;
             q1.add(new int[] { 0, i });
         }
         bfs(heights, pacific, q1);
-
-        boolean atlantic[][] = new boolean[n][m];
-        Queue<int[]> q2 = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            q2.add(new int[] { i, m - 1 });
-            atlantic[i][m - 1] = true;
+        for (int i = 0; i < rows; i++) {
+            q2.add(new int[] { i, cols - 1 });
+            atlantic[i][cols - 1] = true;
         }
-        for (int i = 0; i < m - 1; i++) {
-            q2.add(new int[] { n - 1, i });
-            atlantic[n - 1][i] = true;
+        for (int i = 0; i < cols; i++) {
+            q2.add(new int[] { rows - 1, i });
+            atlantic[rows - 1][i] = true;
         }
         bfs(heights, atlantic, q2);
-
         List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (pacific[i][j] && atlantic[i][j]) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (pacific[i][j] && atlantic[i][j])
                     ans.add(new ArrayList<>(Arrays.asList(i, j)));
-                }
             }
         }
         return ans;
     }
 
     public void bfs(int grid[][], boolean vis[][], Queue<int[]> q) {
-        int n = grid.length, m = grid[0].length;
+        int rows = grid.length, cols = grid[0].length;
         int dx[] = new int[] { -1, 1, 0, 0 };
         int dy[] = new int[] { 0, 0, -1, 1 };
         while (!q.isEmpty()) {
@@ -50,16 +45,16 @@ class Solution {
             for (int i = 0; i < 4; i++) {
                 int nr = dx[i] + r;
                 int nc = dy[i] + c;
-                if(!isValid(nr, nc, n, m) || vis[nr][nc] || grid[nr][nc] < grid[r][c])
+                if (!isValid(nr, nc, rows, cols) || vis[nr][nc] || grid[nr][nc] < grid[r][c])
                     continue;
                 vis[nr][nc] = true;
-                q.add(new int[]{nr, nc});
+                q.add(new int[] { nr, nc });
             }
         }
     }
 
-    public boolean isValid(int r, int c, int n, int m) {
-        if (r < 0 || c < 0 || r >= n || c >= m)
+    public boolean isValid(int r, int c, int rows, int cols) {
+        if (r < 0 || c < 0 || r >= rows || c >= cols)
             return false;
         return true;
     }
