7977007953c1752c2a7a2b8f6d8aa30a05ae7244
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-26 23:21:36 +0530

    Time: 20 ms (91.94%) | Memory: 56.7 MB (52.15%) - LeetSync

--- Premium/google/2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
+++ Premium/google/2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -1,56 +1,71 @@
 class Solution {
     public int latestDayToCross(int row, int col, int[][] cells) {
-        int low = 0;
-        int high = cells.length - 1;
-        int ans = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (canCross(row, col, cells, mid)) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return ans;
-    }
-
-    public boolean canCross(int rows, int cols, int[][] cells, int day) {
-        int grid[][] = new int[rows][cols];
-        for (int i = 0; i < day; i++) {
-            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
-        }
-        Queue<int[]> q = new LinkedList<>();
-        for (int i = 0; i < cols; i++) {
-            if (grid[0][i] == 0) {
-                q.add(new int[] { 0, i });
-                grid[0][i] = 1;
-            }
-        }
-
+        DisjointSet ds = new DisjointSet(row * col + 2);
+        int grid[][] = new int[row][col];
         int dx[] = new int[] { -1, 1, 0, 0 };
         int dy[] = new int[] { 0, 0, -1, 1 };
-        while (!q.isEmpty()) {
-            int cur[] = q.poll();
-            int r = cur[0];
-            int c = cur[1];
-            if (r == rows - 1)
-                return true;
-            for (int i = 0; i < 4; i++) {
-                int nr = dx[i] + r;
-                int nc = dy[i] + c;
-                if (!isValid(nr, nc, rows, cols) || grid[nr][nc] == 1)
+        for (int i = cells.length - 1; i >= 0; i--) {
+            int r = cells[i][0] - 1;
+            int c = cells[i][1] - 1;
+            int curNode = r * col + c;
+            grid[r][c] = 1;
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dx[k];
+                int nc = c + dy[k];
+                if (!isValid(nr, nc, row, col) || grid[nr][nc] == 0)
                     continue;
-                q.add(new int[] { nr, nc });
-                grid[nr][nc] = 1;
+                int adjNode = nr * col + nc;
+                ds.union(curNode, adjNode);
             }
+            if (r == 0) {
+                ds.union(0, curNode);
+            }
+            if (r == row - 1) {
+                ds.union(row * col, curNode);
+            }
+            if (ds.findParent(0) == ds.findParent(row * col))
+                return i;
         }
-        return false;
+        return -1;
     }
 
     public boolean isValid(int r, int c, int n, int m) {
-        if (r >= n || c >= m || r < 0 || c < 0)
+        if (r < 0 || c < 0 || r >= n || c >= m)
             return false;
         return true;
+    }
+}
+
+class DisjointSet {
+    int parent[];
+    int size[];
+
+    DisjointSet(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int findParent(int u) {
+        if (parent[u] == u)
+            return u;
+        return parent[u] = findParent(parent[u]);
+    }
+
+    public void union(int u, int v) {
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if (pu == pv)
+            return;
+        if (size[pu] > size[pv]) {
+            size[pu] += size[pv];
+            parent[pv] = pu;
+        } else {
+            size[pv] += size[pu];
+            parent[pu] = pv;
+        }
     }
 }
