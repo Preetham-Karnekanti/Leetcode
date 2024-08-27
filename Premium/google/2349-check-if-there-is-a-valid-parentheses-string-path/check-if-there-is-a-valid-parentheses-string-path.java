83422eb5bc332995bb1bbfbf3530b58efce284f3
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-27 23:29:22 +0530

    Time: 681 ms (5.26%) | Memory: 119.8 MB (5.26%) - LeetSync

---
+++ Premium/google/2349-check-if-there-is-a-valid-parentheses-string-path/check-if-there-is-a-valid-parentheses-string-path.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean hasValidPath(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        HashMap<String, Boolean> hm = new HashMap<>();
+        return helper(grid, 0, 0, 0, hm);
+    }
+
+    public boolean helper(char grid[][], int i, int j, int open, HashMap<String, Boolean> dp) {
+        int n = grid.length;
+        int m = grid[0].length;
+        String key = i + ":" + j + ":" + open;
+        if (dp.containsKey(key))
+            return dp.get(key);
+        if (i >= n || j >= m || open < 0)
+            return false;
+        if (grid[i][j] == '(')
+            open++;
+        else
+            open--;
+        if (i == n - 1 && j == m - 1) {
+            return open == 0;
+        }
+        boolean val = helper(grid, i + 1, j, open, dp)
+                || helper(grid, i, j + 1, open, dp);
+        dp.put(key, val);
+        return val;
+    }
+}
