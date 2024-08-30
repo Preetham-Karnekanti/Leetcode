dbcd9d4f6c3d06ce94b12a0efdc904721f8083f1
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-31 00:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (33.23%) - LeetSync

--- Premium/google/791-split-bst/split-bst.java
+++ Premium/google/791-split-bst/split-bst.java
@@ -6,17 +6,17 @@ class Solution {
         }
 
         // If root's value is greater than target, recursively split left subtree
-        if (root.val > target) {
-            TreeNode[] left = splitBST(root.left, target);
-            // Attach the right part of the split to root's left subtree
-            root.left = left[1];
-            return new TreeNode[] { left[0], root };
-        } else {
-            // Otherwise, recursively split right subtree
-            TreeNode[] right = splitBST(root.right, target);
+        if (root.val <= target) {
+              TreeNode[] right = splitBST(root.right, target);
             // Attach the left part of the split to root's right subtree
             root.right = right[0];
             return new TreeNode[] { root, right[1] };
+        } else {
+            // Otherwise, recursively split right subtree
+          TreeNode[] left = splitBST(root.left, target);
+            // Attach the right part of the split to root's left subtree
+            root.left = left[1];
+            return new TreeNode[] { left[0], root };
         }
     }
 }
