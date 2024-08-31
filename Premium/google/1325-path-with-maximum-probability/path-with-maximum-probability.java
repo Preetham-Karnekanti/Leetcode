a334f9b73fca1d96e7d62f94b0583d4f48c0042a
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-08-31 09:27:18 +0530

    Time: 37 ms (35.72%) | Memory: 57.1 MB (39.58%) - LeetSync

--- Premium/google/1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ Premium/google/1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,25 +1,33 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            double weight = succProb[i];
-            adj.get(u).add(new Pair(v, weight));
-            adj.get(v).add(new Pair(u, weight));
         }
+        int idx = 0;
+        for (int e[] : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj.get(u).add(new Pair(v, succProb[idx]));
+            adj.get(v).add(new Pair(u, succProb[idx]));
+            idx++;
+        }
+
         PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Double.compare(b.prob, a.prob));
         double probabilities[] = new double[n];
-        probabilities[start_node] = 1.0;
+        probabilities[start_node] = 1;
         pq.add(new Pair(start_node, 1.0));
         while (!pq.isEmpty()) {
             Pair cur = pq.poll();
-            for (Pair ngh : adj.get(cur.node)) {
-                if (cur.prob * ngh.prob > probabilities[ngh.node]) {
-                    probabilities[ngh.node] = cur.prob * ngh.prob;
-                    pq.add(new Pair(ngh.node, cur.prob * ngh.prob));
+            int node = cur.node;
+            double prob = cur.prob;
+
+            for (Pair ngh : adj.get(node)) {
+                int adjNode = ngh.node;
+                double nextProb = ngh.prob;
+                if (prob * nextProb > probabilities[adjNode]) {
+                    probabilities[adjNode] = prob * nextProb;
+                    pq.add(new Pair(adjNode, probabilities[adjNode]));
                 }
             }
         }
