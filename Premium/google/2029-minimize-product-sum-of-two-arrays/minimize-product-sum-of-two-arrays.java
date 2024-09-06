6129042cbc6af00b01d0e9ea5336eac85a0d0ce2
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-09-06 15:39:14 +0530

    Time: 129 ms (9.90%) | Memory: 59.5 MB (81.19%) - LeetSync

--- Premium/google/2029-minimize-product-sum-of-two-arrays/minimize-product-sum-of-two-arrays.java
+++ Premium/google/2029-minimize-product-sum-of-two-arrays/minimize-product-sum-of-two-arrays.java
@@ -1,14 +1,12 @@
 class Solution {
     public int minProductSum(int[] nums1, int[] nums2) {
-        PriorityQueue<Integer> min = new PriorityQueue<>();
         PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i: nums1)
-            min.add(i);
+        Arrays.sort(nums1);
         for(int i: nums2)
             max.add(i);
         int sum = 0;
-        while(!min.isEmpty() && !max.isEmpty()){
-            sum += min.poll()*max.poll();
+        for(int i: nums1){
+            sum += i * max.poll();
         }
         return sum;
     }
