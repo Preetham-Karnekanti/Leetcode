b5d71c28552b2dd0fc45c61aa273d05ea4a34d10
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-09-06 12:59:33 +0530

    Time: 1 ms (100.00%) | Memory: 41.4 MB (56.32%) - LeetSync

--- Premium/google/2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.java
+++ Premium/google/2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.java
@@ -1,20 +1,25 @@
 class Solution {
     public int maxCompatibilitySum(int[][] students, int[][] mentors) {
-        return helper(students, mentors, 0, 0);
+        int dp[][] = new int[students.length][(1 << students.length)];
+        for (int i[] : dp)
+            Arrays.fill(i, -1);
+        return helper(students, mentors, 0, 0, dp);
     }
 
-    public int helper(int[][] students, int[][] mentors, int idx, int mask) {
+    public int helper(int[][] students, int[][] mentors, int idx, int mask, int[][] dp) {
         if (idx == students.length)
             return 0;
+        if (dp[idx][mask] != -1)
+            return dp[idx][mask];
         int maxScore = 0;
         for (int i = 0; i < mentors.length; i++) {
             if (checkbit(mask, i) == false) {
                 int newMask = mask | (1 << i);
                 maxScore = Math.max(maxScore,
-                        getScore(students[idx], mentors[i]) + helper(students, mentors, idx + 1, newMask));
+                        getScore(students[idx], mentors[i]) + helper(students, mentors, idx + 1, newMask, dp));
             }
         }
-        return maxScore;
+        return dp[idx][mask] = maxScore;
     }
 
     public int getScore(int arr1[], int arr2[]) {
