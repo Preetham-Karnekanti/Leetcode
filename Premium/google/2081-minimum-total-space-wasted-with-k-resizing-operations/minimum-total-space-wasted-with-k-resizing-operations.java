78a27de84f2999291f65c4d49b42aa68742be4e2
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-09-07 17:35:44 +0530

    Time: 67 ms (65.96%) | Memory: 41.6 MB (93.62%) - LeetSync

--- Premium/google/2081-minimum-total-space-wasted-with-k-resizing-operations/minimum-total-space-wasted-with-k-resizing-operations.java
+++ Premium/google/2081-minimum-total-space-wasted-with-k-resizing-operations/minimum-total-space-wasted-with-k-resizing-operations.java
@@ -21,7 +21,12 @@ class Solution {
         for (int i = idx; i < nums.length; i++) {
             max = Math.max(max, nums[i]);
             totalSum += nums[i];
-
+            /*
+             * total waste in between [idx...j] would be
+             * summation of (max-nums[idx] + max-nums[idx+1]....max-nums[j])
+             * length would be (j-idx+1) and these summation would be
+             * (j-idx+1) * max upto j - (nums[idx]+nums[idx+1]....+nums[j]
+             */
             int totalSpaceWastedTillJ = (i - idx + 1) * max - totalSum;
             min = Math.min(min, totalSpaceWastedTillJ + helper(nums, i + 1, k - 1));
         }
