c4706644d7555b891f0dc9d2e9836c08e410e57f
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-09-07 17:00:19 +0530

    Time: 46 ms (16.56%) | Memory: 71.7 MB (6.63%) - LeetSync

--- Premium/google/1166-toss-strange-coins/toss-strange-coins.java
+++ Premium/google/1166-toss-strange-coins/toss-strange-coins.java
@@ -7,15 +7,10 @@ class Solution {
     }
 
     public double helper(double prob[], int target, int idx) {
-        if (target == 0) {
-            double ans = 1.0;
-            for (int i = idx; i < prob.length; i++) {
-                ans *= (1 - prob[i]);
-            }
-            return ans;
-        }
+        if (target < 0)
+            return 0;
         if (idx == prob.length)
-            return 0.0;
+            return target == 0 ? 1 : 0;
         if (dp[idx][target] != null)
             return dp[idx][target];
         double heads = prob[idx] * helper(prob, target - 1, idx + 1);
