9b73f3fccf631be40706f2ff867481a462ce8a55
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-09-08 13:11:44 +0530

    Time: 35 ms (82.69%) | Memory: 69.6 MB (74.34%) - LeetSync

---
+++ Premium/google/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minReorder(int n, int[][] connections) {
+        ArrayList<ArrayList<Pair>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int e[] : connections) {
+            int u = e[0];
+            int v = e[1];
+            graph.get(u).add(new Pair(v, 1));
+            graph.get(v).add(new Pair(u, 0));
+        }
+        Queue<Integer> q = new LinkedList<>();
+        int visited[] = new int[n];
+        visited[0] = 1;
+        q.add(0);
+        int count = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (Pair neighbour : graph.get(node)) {
+                int adjNode = neighbour.node;
+                int wt = neighbour.wt;
+                if (visited[adjNode] == 0) {
+                    visited[adjNode] = 1;
+                    q.add(adjNode);
+                    count += wt;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+class Pair {
+    int node, wt;
+
+    Pair(int n, int w) {
+        node = n;
+        wt = w;
+    }
+}
