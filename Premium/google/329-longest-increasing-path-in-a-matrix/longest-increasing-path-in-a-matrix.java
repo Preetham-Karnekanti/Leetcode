e9cd4efb82b70adefa904d6f558e9f94e94a3f50
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-09-09 11:25:43 +0530

    Time: 8 ms (81.04%) | Memory: 44.4 MB (99.26%) - LeetSync

--- Premium/google/329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ Premium/google/329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -10,7 +10,8 @@ class Solution {
             Arrays.fill(i, -1);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                max = Math.max(max, helper(matrix, i, j));
+                if (dp[i][j] == -1)
+                    max = Math.max(max, helper(matrix, i, j));
             }
         }
         return max;
