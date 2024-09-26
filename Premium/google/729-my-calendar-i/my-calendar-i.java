7ac0de5446ebc70b4c143072fd0ad4cc79b4afcb
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-09-26 12:32:51 +0530

    Time: 22 ms (62.47%) | Memory: 45.3 MB (81.32%) - LeetSync

---
+++ Premium/google/729-my-calendar-i/my-calendar-i.java
@@ -0,0 +1,17 @@
+class MyCalendar {
+    TreeMap<Integer, Integer> calendar;
+
+    public MyCalendar() {
+        calendar = new TreeMap<>();
+    }
+
+    public boolean book(int start, int end) {
+        Integer prev = calendar.floorKey(start);
+        Integer next = calendar.ceilingKey(start);
+        if ((prev == null || calendar.get(prev) <= start) && (next == null || end <= next)) {
+            calendar.put(start, end);
+            return true;
+        }
+        return false;
+    }
+}
