fbfb8e6a869e11ac7db969054f543baccfc35d0f
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-10-09 09:10:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (79.36%) - LeetSync

--- Premium/google/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ Premium/google/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,16 +1,21 @@
 class Solution {
+
     public int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(')
-                st.push('(');
-            else {
-                if (!st.isEmpty() && st.peek() == '(')
-                    st.pop();
-                else
-                    st.push(')');
+        int openBrackets = 0;
+        int minAddsRequired = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                openBrackets++;
+            } else {
+                if (openBrackets > 0) {
+                    openBrackets--;
+                } else {
+                    minAddsRequired++;
+                }
             }
         }
-        return st.size();
+
+        return minAddsRequired + openBrackets;
     }
 }
