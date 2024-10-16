cd13c50ae2d773cf52a90806c89575227fdb2d4c
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-10-16 17:51:22 +0530

    Time: 91 ms (25.76%) | Memory: 63.3 MB (7.57%) - LeetSync

---
+++ daily-challenge/3333-find-pattern-in-infinite-stream-i/find-pattern-in-infinite-stream-i.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for an infinite stream.
+ * class InfiniteStream {
+ * public InfiniteStream(int[] bits);
+ * public int next();
+ * }
+ */
+class Solution {
+    public int findPattern(InfiniteStream infiniteStream, int[] pattern) {
+        StringBuilder patternBuilder = new StringBuilder();
+        for (int i : pattern)
+            patternBuilder.append(i);
+        StringBuilder curStream = new StringBuilder();
+        for (int i = 0; i < pattern.length; i++) {
+            curStream.append(infiniteStream.next());
+        }
+        if (patternBuilder.toString().equals(curStream.toString()))
+            return 0;
+        int left = 0;
+        int count = pattern.length;
+        while (left <= (int) 1e5) {
+            count++;
+            left++;
+            curStream.deleteCharAt(0);
+            curStream.append(infiniteStream.next());
+            if (patternBuilder.toString().equals(curStream.toString()))
+                return left;
+        }
+        return -1;
+    }
+}
