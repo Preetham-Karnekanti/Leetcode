69e90142a664860aa740d2e02970c26542092789
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-10-16 13:37:05 +0530

    Time: 139 ms (5.88%) | Memory: 58.2 MB (67.65%) - LeetSync

---
+++ daily-challenge/2003-distinct-numbers-in-each-subarray/distinct-numbers-in-each-subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] distinctNumbers(int[] nums, int k) {
+        int n = nums.length;
+        int ans[] = new int[n - k + 1];
+        int idx = 0;
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < k; i++) {
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        }
+        System.out.println(freq);
+        ans[idx++] = freq.size();
+        int left = 0;
+        for (int right = k; right < nums.length; right++) {
+            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
+            freq.put(nums[left], freq.get(nums[left]) - 1);
+            if (freq.get(nums[left]) == 0) {
+                freq.remove(nums[left]);
+            }
+            left++;
+            ans[idx++] = freq.size();
+        }
+        return ans;
+    }
+}
