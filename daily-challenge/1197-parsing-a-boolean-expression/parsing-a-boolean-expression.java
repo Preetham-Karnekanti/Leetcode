f9c68877ab87309156b747db14844f6288605445
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-10-20 20:33:41 +0530

    Time: 16 ms (21.91%) | Memory: 42.3 MB (75.57%) - LeetSync

--- daily-challenge/1197-parsing-a-boolean-expression/parsing-a-boolean-expression.java
+++ daily-challenge/1197-parsing-a-boolean-expression/parsing-a-boolean-expression.java
@@ -7,31 +7,23 @@ class Solution {
             if (ch != ')')
                 st.push(ch);
             else if (ch == ')') {
-                ArrayList<Character> temp = new ArrayList<>();
-                while (st.peek() != '(')
-                    temp.add(st.pop());
+                boolean hasTrue = false, hasFalse = false;
+                while (st.peek() != '(') {
+                    if (st.peek() == 'f')
+                        hasFalse = true;
+                    if (st.peek() == 't')
+                        hasTrue = true;
+                    st.pop();
+                }
                 st.pop();
                 char op = st.pop();
+                boolean result;
                 if (op == '!') {
-                    st.push(temp.get(0) == 'f' ? 't' : 'f');
+                    st.push(hasTrue ? 'f' : 't');
                 } else if (op == '&') {
-                    char result = 't';
-                    for (char c : temp) {
-                        if (c == 'f') {
-                            result = 'f';
-                            break;
-                        }
-                    }
-                    st.push(result);
-                } else if (op == '|') {
-                    char result = 'f';
-                    for (char c : temp) {
-                        if (c == 't') {
-                            result = 't';
-                            break;
-                        }
-                    }
-                    st.push(result);
+                    st.push(hasFalse ? 'f' : 't');
+                } else {
+                    st.push(hasTrue ? 't' : 'f');
                 }
             }
         }
