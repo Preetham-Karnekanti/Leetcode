a760ed7a5587ea79c1a87a4ff9c821367becce46
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-10-29 10:56:24 +0530

    Time: 3 ms (68.75%) | Memory: 44.2 MB (52.35%) - LeetSync

--- daily-challenge/265-paint-house-ii/paint-house-ii.java
+++ daily-challenge/265-paint-house-ii/paint-house-ii.java
@@ -1,42 +1,22 @@
 class Solution {
-    int dp[][];
-
     public int minCostII(int[][] costs) {
-        dp = new int[costs.length][costs[0].length];
-        for (int i[] : dp)
+        int n = costs.length;
+        int k = costs[0].length;
+        int dp[][] = new int[n][k + 1];
+        for (int[] i : dp)
             Arrays.fill(i, -1);
-        for (int i = 0; i < costs[0].length; i++) {
-            dp[0][i] = costs[0][i];
-        }
-        for (int i = 1; i < costs.length; i++) {
-            for (int color = 0; color < costs[0].length; color++) {
-                int min = Integer.MAX_VALUE;
-                for (int prevColor = 0; prevColor < costs[0].length; prevColor++) {
-                    if (color != prevColor) {
-                        int curCost = costs[i][color] + dp[i - 1][prevColor];
-                        min = Math.min(min, curCost);
-                    }
-                }
-                dp[i][color] = min;
-            }
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < costs[0].length; i++) {
-            min = Math.min(min, dp[costs.length - 1][i]);
-        }
-        return min;
+        return helper(costs, 0, -1, k, dp);
     }
 
-    public int helper(int[][] costs, int idx, int prevColor) {
+    public int helper(int costs[][], int idx, int prevColor, int k, int[][] dp) {
         if (idx == costs.length)
             return 0;
         if (dp[idx][prevColor + 1] != -1)
             return dp[idx][prevColor + 1];
-        int min = Integer.MAX_VALUE;
-        for (int color = 0; color < costs[0].length; color++) {
-            if (color != prevColor) {
-                int curCost = costs[idx][color] + helper(costs, idx + 1, color);
-                min = Math.min(min, curCost);
+        int min = (int) 1e9;
+        for (int i = 0; i < k; i++) {
+            if (prevColor != i) {
+                min = Math.min(min, costs[idx][i] + helper(costs, idx + 1, i, k, dp));
             }
         }
         return dp[idx][prevColor + 1] = min;
