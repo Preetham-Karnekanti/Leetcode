79c4e92d71969f419f71076749e33e9e3f584412
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-10-29 09:44:07 +0530

    Time: 4 ms (20.83%) | Memory: 43.8 MB (93.25%) - LeetSync

--- daily-challenge/265-paint-house-ii/paint-house-ii.java
+++ daily-challenge/265-paint-house-ii/paint-house-ii.java
@@ -2,10 +2,29 @@ class Solution {
     int dp[][];
 
     public int minCostII(int[][] costs) {
-        dp = new int[costs.length][costs[0].length + 1];
+        dp = new int[costs.length][costs[0].length];
         for (int i[] : dp)
             Arrays.fill(i, -1);
-        return helper(costs, 0, -1);
+        for (int i = 0; i < costs[0].length; i++) {
+            dp[0][i] = costs[0][i];
+        }
+        for (int i = 1; i < costs.length; i++) {
+            for (int color = 0; color < costs[0].length; color++) {
+                int min = Integer.MAX_VALUE;
+                for (int prevColor = 0; prevColor < costs[0].length; prevColor++) {
+                    if (color != prevColor) {
+                        int curCost = costs[i][color] + dp[i - 1][prevColor];
+                        min = Math.min(min, curCost);
+                    }
+                }
+                dp[i][color] = min;
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < costs[0].length; i++) {
+            min = Math.min(min, dp[costs.length - 1][i]);
+        }
+        return min;
     }
 
     public int helper(int[][] costs, int idx, int prevColor) {
