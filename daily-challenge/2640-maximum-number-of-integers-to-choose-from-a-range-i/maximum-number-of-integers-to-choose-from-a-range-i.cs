83c64e160c660e8a5d51fe0054a4f99b6405e320
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-12-06 21:33:36 +0530

    Time: 7 ms (76.92%) | Memory: 61.4 MB (100.00%) - LeetSync

--- daily-challenge/2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cs
+++ daily-challenge/2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public int MaxCount(int[] banned, int n, int maxSum) {
-HashSet<int> b = new HashSet<int>();
-
+// HashSet<int> b = new HashSet<int>();
+bool[] b = new bool[10001];
 foreach(int a in banned)
 {
-    b.Add(a);
+    b[a] = true;
 }
         int count=0, sum=0;
         for(int i=1; i<= n; i++)
         {
-            if(!b.Contains(i))
+            if(!b[i])
             {
                 
                 sum += i;
