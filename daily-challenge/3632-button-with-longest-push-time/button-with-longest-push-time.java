f23db2c14232a59744dd08acb8261fa17d22d62f
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-12-17 11:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (90.44%) - LeetSync

--- daily-challenge/3632-button-with-longest-push-time/button-with-longest-push-time.java
+++ daily-challenge/3632-button-with-longest-push-time/button-with-longest-push-time.java
@@ -1,21 +1,23 @@
 class Solution {
     public int buttonWithLongestTime(int[][] events) {
-        int time[] = new int[(int) 1e5 + 1];
-        int max = events[0][1];
-        int idx = events[0][0];
-        time[idx] = events[0][1];
+        int longestTime = events[0][1];
+        int finalIndex = events[0][0];
+
         for (int i = 1; i < events.length; i++) {
-            int curIdx = events[i][0];
-            time[curIdx] = Math.max(time[curIdx], events[i][1] - events[i - 1][1]);
-            if (time[curIdx] >= max) {
-                if (time[curIdx] > max) {
-                    max = time[curIdx];
-                    idx = curIdx;
-                } else {
-                    idx = Math.min(idx, curIdx);
-                }
+            int indx = events[i][0];
+            int time = events[i][1];
+
+            int prevTime = events[i - 1][1];
+
+            if (time - prevTime > longestTime) {
+                finalIndex = indx;
+                longestTime = time - prevTime;
+            } else if (time - prevTime == longestTime) {
+
+                finalIndex = Math.min(finalIndex, indx);
             }
+
         }
-        return idx;
+        return finalIndex;
     }
 }
