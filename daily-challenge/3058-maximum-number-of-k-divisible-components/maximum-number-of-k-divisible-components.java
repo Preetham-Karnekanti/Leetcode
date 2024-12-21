043116f4b707835a8efb16733add1befc9b91b09
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-12-21 12:27:39 +0530

    Time: 22 ms (93.51%) | Memory: 64.3 MB (20.78%) - LeetSync

---
+++ daily-challenge/3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
@@ -0,0 +1,34 @@
+class Solution {
+    int ans = 0;
+
+    public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+        ans = 0;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+        for (int e[] : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        dfs(adj, 0, new boolean[n], k, values);
+        return ans == 0 ? 1 : ans;
+    }
+
+    public long dfs(ArrayList<ArrayList<Integer>> adj, int node, boolean[] vis, int k, int[] values) {
+        vis[node] = true;
+        long val = values[node];
+
+        for (int ngh : adj.get(node)) {
+            if (!vis[ngh]) {
+                vis[ngh] = true;
+                val += dfs(adj, ngh, vis, k, values);
+            }
+        }
+        if (val % k == 0) {
+            ans++;
+        }
+        return val;
+    }
+}
