fdb20b36da429b9afb39226e14f2664762cb35f2
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2024-12-23 10:06:15 +0530

    Time: 7 ms (100.00%) | Memory: 46.7 MB (84.54%) - LeetSync

--- daily-challenge/1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.java
+++ daily-challenge/1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.java
@@ -8,33 +8,33 @@
  * }
  */
 class Solution {
+    boolean foundP, foundQ;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        boolean hasP = find(root, p);
-        boolean hasQ = find(root, q);
-        if (hasP == false || hasQ == false)
-            return null;
-        return helper(root, p, q);
+        TreeNode res = helper(root, p, q);
+        if (foundP && foundQ)
+            return res;
+        return null;
     }
 
-    public boolean find(TreeNode root, TreeNode key) {
-        if (root == null)
-            return false;
-        if (root == key)
-            return true;
-        return find(root.left, key) || find(root.right, key);
-    }
-
-    public TreeNode helper(TreeNode root, TreeNode p, TreeNode q){
-        if (root == null || root == p || root == q)
+    public TreeNode helper(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
             return root;
+        }
         TreeNode left = helper(root.left, p, q);
         TreeNode right = helper(root.right, p, q);
-        if (left != null && right != null)
+        if (root == p) {
+            foundP = true;
             return root;
-        if (left == null)
-            return right;
-        if (right == null)
-            return left;
-        return null;
+        }
+        if (root == q) {
+            foundQ = true;
+            return root;
+        }
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return left == null ? right : left;
     }
 }
