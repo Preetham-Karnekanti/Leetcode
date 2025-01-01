43f6627e1348680e580aa3e84dac093d4505df95
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-01-01 07:32:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (12.11%) - LeetSync

--- daily-challenge/1345-perform-string-shifts/perform-string-shifts.java
+++ daily-challenge/1345-perform-string-shifts/perform-string-shifts.java
@@ -1,24 +1,22 @@
 class Solution {
-    public String stringShift(String s, int[][] shift) {
-        Deque<Character> q = new ArrayDeque();
-        int n = s.length();
-        for (int i = 0; i < s.length(); i++)
-            q.add(s.charAt(i));
-        for (int ops[] : shift) {
-            int direction = ops[0];
-            int k = ops[1] % n;
-            for (int i = 0; i < k; i++) {
-                if (direction == 0) {
-                    q.addLast(q.pollFirst());
-                } else {
-                    q.addFirst(q.pollLast());
-                }
-            }
+    public String stringShift(String string, int[][] shift) {
+
+        int[] overallShifts = new int[2];
+        for (int[] move : shift) {
+            overallShifts[move[0]] += move[1];
         }
-        StringBuilder sb = new StringBuilder();
-        while (!q.isEmpty()) {
-            sb.append(q.poll());
+        int leftShifts = overallShifts[0];
+        int rightShifts = overallShifts[1];
+
+        int len = string.length();
+        if (leftShifts > rightShifts) {
+            leftShifts = (leftShifts - rightShifts) % len;
+            string = string.substring(leftShifts) + string.substring(0, leftShifts);
+        } else if (rightShifts > leftShifts) {
+            rightShifts = (rightShifts - leftShifts) % len;
+            string = string.substring(len - rightShifts) + string.substring(0, len - rightShifts);
         }
-        return sb.toString();
+
+        return string;
     }
 }
