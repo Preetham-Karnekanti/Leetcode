38b1a80c8edcc81319ed5c12ae076bf922eb3f24
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-01-05 21:07:30 +0530

    Time: 34 ms (100.00%) | Memory: 46.4 MB (100.00%) - LeetSync

--- daily-challenge/3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.java
+++ daily-challenge/3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.java
@@ -2,17 +2,18 @@ class Solution {
     public long calculateScore(String s) {
         long ans = 0;
         String mirror = "zyxwvutsrqponmlkjihgfedcba";
-        HashMap<Character, PriorityQueue<Integer>> hm = new HashMap<>();
+        HashMap<Character, List<Integer>> hm = new HashMap<>();
         for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
             char mirrorChar = mirror.charAt(ch-'a');
-            PriorityQueue<Integer> pq = hm.getOrDefault(mirrorChar, new PriorityQueue());
-            hm.putIfAbsent(ch, new PriorityQueue<>((a,b)-> b-a));
+            List<Integer> pq = hm.getOrDefault(mirrorChar, new ArrayList());
+            hm.putIfAbsent(ch, new ArrayList<>());
            if(pq.isEmpty()){
             hm.get(ch).add(i);
             continue;
            }
-           ans += (i - pq.poll());
+           ans += (i - pq.get(pq.size()-1));
+           pq.remove(pq.size()-1);
         }
         return ans;
     }
