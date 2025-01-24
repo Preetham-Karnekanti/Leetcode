3619743ad1127665360bbf92107857c359b141f5
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-01-24 08:59:47 +0530

    Time: 4 ms (99.02%) | Memory: 55.8 MB (15.73%) - LeetSync

--- daily-challenge/820-find-eventual-safe-states/find-eventual-safe-states.java
+++ daily-challenge/820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,33 +1,35 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = graph.length;
-        int indegree[] = new int[n];
-        for (int i = 0; i < n; i++)
-            adj.add(new ArrayList<>());
-        for (int i = 0; i < graph.length; i++) {
-            for (int node : graph[i]) {
-                adj.get(node).add(i);
-                indegree[i]++;
+        int[] state = new int[n]; // 0 = unvisited, 1 = visiting, 2 = safe
+        List<Integer> safeNodes = new ArrayList<>();
+        
+        // Perform DFS for each node
+        for (int i = 0; i < n; i++) {
+            if (dfs(graph, state, i)) {
+                safeNodes.add(i);
             }
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0;i<n;i++){
-            if(indegree[i] == 0)
-                q.add(i);
+        
+        return safeNodes;
+    }
+    
+    private boolean dfs(int[][] graph, int[] state, int node) {
+        if (state[node] != 0) {
+            // Return true if the node is safe, false otherwise
+            return state[node] == 2;
         }
-        ArrayList<Integer> ans = new ArrayList<>();
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            ans.add(cur);
-            for (int ngh : adj.get(cur)) {
-                indegree[ngh]--;
-                if (indegree[ngh] == 0)
-                    q.add(ngh);
+        
+        state[node] = 1; // Mark as visiting
+        
+        for (int neighbor : graph[node]) {
+            // If a neighbor is not safe or part of a cycle, mark this node unsafe
+            if (!dfs(graph, state, neighbor)) {
+                return false;
             }
         }
-        Collections.sort(ans);
-        return ans;
+        
+        state[node] = 2; // Mark as safe
+        return true;
     }
 }
