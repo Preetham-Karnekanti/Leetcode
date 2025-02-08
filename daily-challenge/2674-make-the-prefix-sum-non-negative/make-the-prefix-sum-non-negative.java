37832cf76753c47a8f7b1e200f66fb7463b1e36e
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-02-08 07:28:43 +0530

    Time: 10 ms (47.92%) | Memory: 56.8 MB (97.92%) - LeetSync

--- daily-challenge/2674-make-the-prefix-sum-non-negative/make-the-prefix-sum-non-negative.java
+++ daily-challenge/2674-make-the-prefix-sum-non-negative/make-the-prefix-sum-non-negative.java
@@ -4,7 +4,8 @@ class Solution {
         int ops = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
         for (int i = 0; i < nums.length; i++) {
-            pq.add(nums[i]);
+            if (nums[i] < 0)
+                pq.add(nums[i]);
             sum += nums[i];
             if (sum < 0) {
                 sum -= pq.poll();
