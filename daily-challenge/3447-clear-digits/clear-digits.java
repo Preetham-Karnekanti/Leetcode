316e39fc4ff5fcba7afd80303246eb653b789b5c
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-02-10 08:40:06 +0530

    Time: 3 ms (38.21%) | Memory: 42.8 MB (33.17%) - LeetSync

---
+++ daily-challenge/3447-clear-digits/clear-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                if (st.isEmpty() == false && st.peek() >= 'a' && st.peek() <= 'z')
+                    st.pop();
+            } else {
+                st.push(s.charAt(i));
+            }
+        }
+        while (!st.isEmpty())
+            sb.append(st.pop());
+        return sb.reverse().toString();
+    }
+}
