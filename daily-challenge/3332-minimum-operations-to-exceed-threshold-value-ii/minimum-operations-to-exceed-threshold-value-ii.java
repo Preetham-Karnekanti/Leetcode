726fc3577950ef3983df13e7bc4bfd26c4b08ca0
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-02-13 05:41:16 +0530

    Time: 156 ms (17.70%) | Memory: 70.4 MB (80.25%) - LeetSync

---
+++ daily-challenge/3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long> pq = new PriorityQueue<>((a, b) -> Long.compare(a, b));
+        for (int i : nums) {
+            pq.add((long) i);
+        }
+        int ops = 0;
+        while (pq.size() >= 2 && pq.peek() < k) {
+            long a = pq.poll();
+            long b = pq.poll();
+            pq.add(a * 2 + b);
+            ops++;
+        }
+        return ops;
+    }
+}
