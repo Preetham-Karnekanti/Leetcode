928ae8ecdb672ecd9579afc12914a96a45a96c84
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-02-18 07:23:13 +0530

    Time: 1 ms (75.25%) | Memory: 41.7 MB (62.62%) - LeetSync

---
+++ daily-challenge/2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public String smallestNumber(String pattern) {
+        StringBuilder result = new StringBuilder();
+        Stack<Integer> numStack = new Stack<>();
+
+        for (int index = 0; index <= pattern.length(); index++) {
+            numStack.push(index + 1);
+
+            if (index == pattern.length() || pattern.charAt(index) == 'I') {
+                while (!numStack.isEmpty()) {
+                    result.append(numStack.pop());
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
