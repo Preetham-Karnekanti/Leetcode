7aec2ca10d65228fa3be1091c6c7b4bb21285da6
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-02-27 21:57:19 +0530

    Time: 1 ms (69.52%) | Memory: 41.4 MB (96.89%) - LeetSync

--- daily-challenge/2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ daily-challenge/2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -7,12 +7,15 @@ class Solution {
         for (int index = 0; index <= pattern.length(); index++) {
             numStack.push(index + 1);
 
-            if (index == pattern.length() || pattern.charAt(index) == 'I') {
+            if (index < pattern.length() && pattern.charAt(index) == 'I') {
                 while (!numStack.isEmpty()) {
                     result.append(numStack.pop());
                 }
             }
         }
+
+        while(!numStack.isEmpty())
+              result.append(numStack.pop());
 
         return result.toString();
     }
