dc2dae4b0b498597e30a653f1958f234a6c231e6
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-03-14 08:47:29 +0530

    Time: 31 ms (45.70%) | Memory: 63.1 MB (69.89%) - LeetSync

---
+++ daily-challenge/1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        long low = 1;
+        long high = 0;
+        long ans = 0;
+        for (int i : candies)
+            high = Math.max(high, i);
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            if (isPossible(candies, mid, k)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return (int)ans;
+    }
+
+    public boolean isPossible(int arr[], long perChild, long total) {
+        long count = 0;
+        for (int i : arr) {
+            count += i / perChild;
+            if (count >= total)
+                return true;
+        }
+        return false;
+    }
+}
