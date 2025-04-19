6bb619485eda4e27c825676fe2743c65eefb4357
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-04-19 09:18:29 +0530

    Time: 23 ms (100.00%) | Memory: 57.2 MB (84.51%) - LeetSync

--- daily-challenge/2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ daily-challenge/2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -1,44 +1,22 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
         Arrays.sort(nums);
-        long count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            long x = bs1(nums, i + 1, lower - nums[i]);
-            long y = bs2(nums, i + 1, upper - nums[i]);
-            count += (y - x);
-        }
-        return count;
+        return count(nums, upper) - count(nums, lower - 1);
     }
 
-    public int bs1(int nums[], int idx, int target) {
-        int low = idx;
-        int high = nums.length - 1;
-        int ans = 0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] >= target) {
-                high = mid - 1;
-            } else {
-                ans = mid;
-                low = mid + 1;
+    public long count(int nums[], int value) {
+        int left = 0;
+        int right = nums.length - 1;
+        long ans = 0;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum > value)
+                right--;
+            else {
+                ans += (right - left);
+                left++;
             }
         }
-        return low;
-    }
-
-    public int bs2(int nums[], int idx, int target) {
-        int low = idx;
-        int high = nums.length - 1;
-        int ans = 0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] <= target) {
-                low = mid + 1;
-            } else {
-                ans = mid;
-                high = mid - 1;
-            }
-        }
-        return low;
+        return ans;
     }
 }
