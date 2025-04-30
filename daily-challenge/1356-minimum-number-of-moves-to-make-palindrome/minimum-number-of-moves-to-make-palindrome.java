dd41d0438a3f9da1bcbbc29e69d93f3f7cf75fa0
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-04-30 10:08:22 +0530

    Time: 15 ms (25.91%) | Memory: 45 MB (10.63%) - LeetSync

---
+++ daily-challenge/1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minMovesToMakePalindrome(String s) {
+        int ans = 0;
+        while (s.length() > 0) {
+            int i = s.indexOf(s.charAt(s.length() - 1));
+            if (i == s.length() - 1) {
+                ans += i / 2;
+                s = s.substring(0, s.length() - 1);
+            } else {
+                ans += i;
+                s = s.substring(0, i) + s.substring(i + 1, s.length() - 1);
+            }
+        }
+        return ans;
+    }
+}
