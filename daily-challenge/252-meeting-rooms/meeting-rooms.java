1a21613a3a8427f153e8fce8dbb658d3b90cd1b1
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-04-30 21:07:16 +0530

    Time: 5 ms (97.12%) | Memory: 45.4 MB (99.39%) - LeetSync

---
+++ daily-challenge/252-meeting-rooms/meeting-rooms.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        int max = 0;
+        int n = intervals.length;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, intervals[i][1]);
+        }
+        int timeline[] = new int[max + 1];
+        for (int i = 0; i < n; i++) {
+            timeline[intervals[i][0]]++;
+            timeline[intervals[i][1]]--;
+        }
+        for (int i = 1; i <= max; i++) {
+            timeline[i] += timeline[i - 1];
+            if (timeline[i] > 1)
+                return false;
+        }
+        return true;
+    }
+}
