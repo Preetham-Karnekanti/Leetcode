de1a1820c5a7154bb158ddf9e9820fff8506b079
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-06-24 20:07:29 +0530

    Time: 2 ms (97.25%) | Memory: 45 MB (44.63%) - LeetSync

--- daily-challenge/2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
+++ daily-challenge/2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
@@ -1,27 +1,18 @@
 class Solution {
-    public List findKDistantIndices(int[] nums, int key, int k) {
+
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        List<Integer> res = new ArrayList<>();
+        int r = 0; // unjudged minimum index
         int n = nums.length;
-        int[] diffarr = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == key) {
-                int l = i - k;
-                int r = i + k + 1;
-                if (l < 0)
-                    diffarr[0] += 1;
-                else
-                    diffarr[l] += 1;
-                if (r < n)
-                    diffarr[r] -= 1;
+        for (int j = 0; j < n; ++j) {
+            if (nums[j] == key) {
+                int l = Math.max(r, j - k);
+                r = Math.min(n - 1, j + k) + 1;
+                for (int i = l; i < r; ++i) {
+                    res.add(i);
+                }
             }
         }
-        List ans = new ArrayList<>();
-        if (diffarr[0] > 0)
-            ans.add(0);
-        for (int i = 1; i < n; i++) {
-            diffarr[i] += diffarr[i - 1];
-            if (diffarr[i] > 0)
-                ans.add(i);
-        }
-        return ans;
+        return res;
     }
 }
