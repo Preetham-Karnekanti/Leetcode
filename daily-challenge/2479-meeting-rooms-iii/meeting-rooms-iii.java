1cccda7245c8ee151896c3852adf7312f93ba3de
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-07-19 20:49:00 +0530

    Time: 51 ms (83.68%) | Memory: 101.3 MB (9.10%) - LeetSync

---
+++ daily-challenge/2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
+        long finishTime[] = new long[n];
+        int count[] = new int[n];
+        for (int i = 0; i < meetings.length; i++) {
+            int start = meetings[i][0];
+            int end = meetings[i][1];
+            long minEnd = Long.MAX_VALUE;
+            int minIdx = -1;
+            boolean hasEmptyRoom = false;
+            for (int j = 0; j < n; j++) {
+                if (finishTime[j] < minEnd) {
+                    minEnd = finishTime[j];
+                    minIdx = j;
+                }
+                if (finishTime[j] <= start) {
+                    finishTime[j] = end;
+                    count[j]++;
+                    hasEmptyRoom = true;
+                    break;
+                }
+            }
+            if (hasEmptyRoom == false) {
+                finishTime[minIdx] += (end - start);
+                count[minIdx]++;
+            }
+        }
+        int max = 0;
+        int roomNo = -1;
+        for (int i = 0; i < n; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                roomNo = i;
+            }
+        }
+        return roomNo;
+    }
+}
