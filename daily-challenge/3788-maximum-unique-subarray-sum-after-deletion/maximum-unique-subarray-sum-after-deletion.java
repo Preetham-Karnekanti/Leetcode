4f1b008e1b689d6880a2400b6aaac9e875e59ed6
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-07-25 13:29:46 +0530

    Time: 2 ms (74.06%) | Memory: 42.6 MB (50.31%) - LeetSync

--- daily-challenge/3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
+++ daily-challenge/3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
@@ -2,17 +2,17 @@ class Solution {
     public int maxSum(int[] nums) {
         HashSet<Integer> hs = new HashSet<>();
         int answer = 0;
-        int nge = 0;
+        int neg = 0;
         int max = -102;
         for (int i : nums) {
             max = Math.max(i, max);
             if (i < 0)
-                nge++;
+                neg++;
             if (i <= 0 || hs.contains(i))
                 continue;
             hs.add(i);
             answer += i;
         }
-        return nge == nums.length ? max : answer;
+        return neg == nums.length ? max : answer;
     }
 }
