23e9e3019ca81707a07f2edc3acbbfaa3b27351e
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-08-17 22:30:16 +0530

    Time: 5 ms (78.94%) | Memory: 42.1 MB (80.18%) - LeetSync

--- daily-challenge/768-partition-labels/partition-labels.java
+++ daily-challenge/768-partition-labels/partition-labels.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        int lastOcc[] = new int[26];
+        int last[] = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            lastOcc[s.charAt(i) - 'a'] = i;
+            last[s.charAt(i) - 'a'] = i;
         }
-        ArrayList<Integer> al = new ArrayList<>();
+        ArrayList<Integer> answer = new ArrayList<>();
         int i = 0;
-        int n = s.length();
-        while (i < n) {
-            int right = lastOcc[s.charAt(i) - 'a'];
+        while (i < s.length()) {
+            int maxIdx = last[s.charAt(i) - 'a'];
             int j = i;
-            while (j < right) {
-                right = Math.max(right, lastOcc[s.charAt(j) - 'a']);
+            while (j < s.length() && j < maxIdx) {
+                maxIdx = Math.max(maxIdx, last[s.charAt(j) - 'a']);
                 j++;
             }
-            al.add(j - i + 1);
-            i = j + 1;
+            answer.add(j - i + 1);
+            i = j;
+            i++;
         }
-        return al;
+        return answer;
     }
 }
