7c539b7bc1962d41ccf33e10310209386c99e324
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-08-19 09:05:53 +0530

    Time: 3 ms (94.03%) | Memory: 61.4 MB (81.82%) - LeetSync

--- daily-challenge/2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
+++ daily-challenge/2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
@@ -1,14 +1,16 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
         long ans = 0;
-        int count = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                count = 0;
-            } else {
-                count++;
-                ans += count;
-            }
+            if (nums[i] != 0)
+                continue;
+            int j = i;
+            while (j < nums.length && nums[j] == 0)
+                j++;
+            long len = j - i;
+            long subarrays = len * (len + 1) / 2;
+            ans += subarrays;
+            i = j - 1;
         }
         return ans;
     }
