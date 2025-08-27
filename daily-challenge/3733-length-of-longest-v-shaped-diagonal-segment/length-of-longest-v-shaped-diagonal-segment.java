e514d17d7cb4372d10e6ea4bd378ce322e286547
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2025-08-27 09:13:17 +0530

    Time: 492 ms (77.63%) | Memory: 127.8 MB (11.84%) - LeetSync

--- daily-challenge/3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.java
+++ daily-challenge/3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.java
@@ -1,35 +1,26 @@
 class Solution {
-
-    private static final int[][] DIRS = {
-        { 1, 1 },
-        { 1, -1 },
-        { -1, -1 },
-        { -1, 1 },
+    int[][] dir = {
+            { 1, 1 },
+            { 1, -1 },
+            { -1, -1 },
+            { -1, 1 }
     };
-    private int[][][][] memo;
-    private int[][] grid;
-    private int m, n;
+    int dp[][][][];
 
     public int lenOfVDiagonal(int[][] grid) {
-        this.grid = grid;
-        this.m = grid.length;
-        this.n = grid[0].length;
-        this.memo = new int[m][n][4][2];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < 4; k++) {
-                    Arrays.fill(memo[i][j][k], -1);
-                }
+        int res = 0;
+        dp = new int[grid.length][grid[0].length][4][2];
+        for (int i[][][] : dp) {
+            for (int j[][] : i) {
+                for (int k[] : j)
+                    Arrays.fill(k, -1);
             }
         }
-
-        int res = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    for (int direction = 0; direction < 4; direction++) {
-                        res = Math.max(res, dfs(i, j, direction, true, 2) + 1);
+                    for (int direction = 0; direction < dir.length; direction++) {
+                        res = Math.max(res, dfs(grid, i, j, direction, 0, 2) + 1);
                     }
                 }
             }
@@ -37,29 +28,19 @@ class Solution {
         return res;
     }
 
-    private int dfs(int cx, int cy, int direction, boolean turn, int target) {
-        int nx = cx + DIRS[direction][0];
-        int ny = cy + DIRS[direction][1];
-        /* If it goes beyond the boundary or the next node's value is not the target value, then return */
-        if (nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != target) {
+    public int dfs(int grid[][], int r, int c, int direction, int canTurn, int next) {
+        int nr = r + dir[direction][0];
+        int nc = c + dir[direction][1];
+
+        if (nr < 0 || nc < 0 || nr >= grid.length || nc >= grid[0].length || grid[nr][nc] != next) {
             return 0;
         }
-
-        int turnInt = turn ? 1 : 0;
-        if (memo[nx][ny][direction][turnInt] != -1) {
-            return memo[nx][ny][direction][turnInt];
+        if (dp[nr][nc][direction][canTurn] != -1)
+            return dp[nr][nc][direction][canTurn];
+        int max = 1 + dfs(grid, nr, nc, direction, canTurn, 2 - next);
+        if (canTurn == 0) {
+            max = Math.max(max, 1 + dfs(grid, nr, nc, (direction + 1) % 4, 1, 2 - next));
         }
-
-        /* Continue walking in the original direction. */
-        int maxStep = dfs(nx, ny, direction, turn, 2 - target);
-        if (turn) {
-            /* Clockwise rotate 90 degrees turn */
-            maxStep = Math.max(
-                maxStep,
-                dfs(nx, ny, (direction + 1) % 4, false, 2 - target)
-            );
-        }
-        memo[nx][ny][direction][turnInt] = maxStep + 1;
-        return maxStep + 1;
+        return dp[nr][nc][direction][canTurn] = max;
     }
 }
