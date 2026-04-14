33a880bd9e3d8a25c90651bdb082db3ad88526ba
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2026-04-14 08:19:40 +0530

    Time: 2 ms (35.19%) | Memory: 43.1 MB (29.91%) - LeetSync

---
+++ daily-challenge/394-decode-string/decode-string.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+        
+        String current = "";
+        int k = 0;
+        
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            } 
+            else if (ch == '[') {
+                countStack.push(k);
+                strStack.push(current);
+                k = 0;
+                current = "";
+            } 
+            else if (ch == ']') {
+                int repeat = countStack.pop();
+                String prev = strStack.pop();
+                
+                StringBuilder temp = new StringBuilder(prev);
+                for (int i = 0; i < repeat; i++) {
+                    temp.append(current);
+                }
+                current = temp.toString();
+            } 
+            else {
+                current += ch;
+            }
+        }
+        
+        return current;
+    }
+}
