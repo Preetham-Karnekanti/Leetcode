f47473db3a2de128ecda528fab189e8b4fd58db2
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2026-05-12 20:47:51 +0530

    Time: 24 ms (55.77%) | Memory: 113.9 MB (67.31%) - LeetSync

--- daily-challenge/1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
+++ daily-challenge/1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
@@ -1,30 +1,29 @@
 class Solution {
     public int minimumEffort(int[][] tasks) {
-        Arrays.sort(tasks, (a, b) -> {
-            return (b[1] - b[0]) - (a[1] - a[0]);
-        });
 
-        int low = 0;
-        int high = (int) 1e9;
-        int ans = -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (check(tasks, mid)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        Arrays.sort(tasks, (a, b) ->
+            Integer.compare(
+                (b[1] - b[0]),
+                (a[1] - a[0])
+            )
+        );
+
+        int energy = 0;
+        int current = 0;
+
+        for (int[] task : tasks) {
+
+            int actual = task[0];
+            int minimum = task[1];
+
+            if (current < minimum) {
+                energy += (minimum - current);
+                current = minimum;
             }
-        }
-        return ans;
-    }
 
-    public boolean check(int[][] tasks, int max) {
-        for (int i = 0; i < tasks.length; i++) {
-            if (tasks[i][1] > max)
-                return false;
-            max -= tasks[i][0];
+            current -= actual;
         }
-        return true;
+
+        return energy;
     }
 }
