bc2a7211cfae2bdb5e726ca7939ef7960d7aba9f
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2026-05-24 13:29:19 +0530

    Time: 10 ms (87.43%) | Memory: 46.1 MB (88.29%) - LeetSync

--- daily-challenge/1466-jump-game-v/jump-game-v.java
+++ daily-challenge/1466-jump-game-v/jump-game-v.java
@@ -1,33 +1,32 @@
 class Solution {
     int dp[];
-
     public int maxJumps(int[] arr, int d) {
-        int max = 0;
         dp = new int[arr.length];
         Arrays.fill(dp, -1);
+        int ans = -1;
         for (int i = 0; i < arr.length; i++) {
-            max = Math.max(max, helper(arr, i, d));
+            ans = Math.max(ans, helper(arr, i, d));
         }
-        return max;
+        return ans;
     }
 
     public int helper(int arr[], int i, int d) {
-        if (i >= arr.length || i < 0)
-            return 0;
-        if (dp[i] != -1)
+        if(dp[i] != -1)
             return dp[i];
         int ans = 1;
         for (int j = i + 1; j <= i + d && j < arr.length; j++) {
-            if (arr[i] > arr[j])
+            if (arr[i] > arr[j]) {
                 ans = Math.max(ans, 1 + helper(arr, j, d));
-            else
+            } else {
                 break;
+            }
         }
-        for (int j = i - 1; j >= 0 && j >= i - d; j--) {
-            if (arr[i] > arr[j])
+        for (int j = i - 1; j >= i - d && j >= 0; j--) {
+            if (arr[i] > arr[j]) {
                 ans = Math.max(ans, 1 + helper(arr, j, d));
-            else
+            } else{
                 break;
+            }
         }
         return dp[i] = ans;
     }
