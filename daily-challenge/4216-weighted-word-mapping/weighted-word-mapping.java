8f4b5d2fc659d9e25f0f9611ec125e0a9da8906a
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2026-06-13 09:02:08 +0530

    Time: 2 ms (96.85%) | Memory: 46.3 MB (89.99%) - LeetSync

--- daily-challenge/4216-weighted-word-mapping/weighted-word-mapping.java
+++ daily-challenge/4216-weighted-word-mapping/weighted-word-mapping.java
@@ -1,14 +1,14 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         for (String word : words) {
             int weight = 0;
             for (char ch : word.toCharArray()) {
                 weight += weights[ch - 'a'];
             }
             weight %= 26;
-            answer += (char) (25 - weight + 'a');
+            answer.append((char) (25 - weight + 'a'));
         }
-        return answer;
+        return answer.toString();
     }
 }
