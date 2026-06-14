dfbd5923c14b3a8d14daffaab3a17668f5ea1d48
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2026-06-14 08:19:59 +0530

    Time: 21 ms (6.35%) | Memory: 112.1 MB (20.11%) - LeetSync

--- daily-challenge/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ daily-challenge/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -1,46 +1,17 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+    int res;
+    ListNode left;
+
     public int pairSum(ListNode head) {
-        ListNode mid = findMid(head);
-        ListNode secondHalf = mid.next;
-        mid.next = null;
-        secondHalf = reverse(secondHalf);
-        int max = Integer.MIN_VALUE;
-        while (head != null && secondHalf != null) {
-            max = Math.max(max, head.val + secondHalf.val);
-            head = head.next;
-            secondHalf = secondHalf.next;
-        }
-        return max;
+        left = head;
+        dfs(head);
+        return res;
     }
 
-    public ListNode findMid(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head.next;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
-    public ListNode reverse(ListNode head) {
-        ListNode cur = head, prev = null, next = null;
-        while (cur != null) {
-            next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
-        }
-        return prev;
+    private void dfs(ListNode right) {
+        if (right.next != null)
+            dfs(right.next);
+        res = Math.max(res, left.val + right.val);
+        left = left.next;
     }
 }
