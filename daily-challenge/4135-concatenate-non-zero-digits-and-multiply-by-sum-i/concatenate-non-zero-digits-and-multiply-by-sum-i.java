4d54e70879d95bc4184fbc1ab179687724684ec2
Preetham-Karnekanti <100109342+Preetham-Karnekanti@users.noreply.github.com>
2026-07-07 08:00:16 +0530

    Time: 1 ms (99.85%) | Memory: 42.6 MB (67.71%) - LeetSync

--- daily-challenge/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ daily-challenge/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,14 +1,18 @@
 class Solution {
+
     public long sumAndMultiply(int n) {
-        String num = String.valueOf(n);
-        long ans = 0;
+        long x = 0;
         long sum = 0;
-        for (int i = 0; i < num.length(); i++) {
-            if (num.charAt(i) != '0') {
-                sum += (num.charAt(i) - '0');
-                ans = ans * 10 + (num.charAt(i) - '0');
+        long pow10 = 1;
+        while (n > 0) {
+            int d = n % 10;
+            sum += d;
+            if (d > 0) {
+                x += d * pow10;
+                pow10 *= 10;
             }
+            n /= 10;
         }
-        return sum * ans;
+        return x * sum;
     }
 }
